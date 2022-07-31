1d530d55b653b15d16a91c17e2d948147ae02144
Deepfried Chips <mihaititiriga07@gmail.com>
2022-07-31 23:52:58 +0300

    I hate websockets.

--- src/Powercord/plugins/pc-websockets/index.js
+++ src/Powercord/plugins/pc-websockets/index.js
@@ -1,30 +1,7 @@
 const { Plugin } = require('powercord/entities');
-const { getModule } = require('powercord/webpack');
-const { inject, uninject } = require('powercord/injector');
 
-module.exports = class WSLinks extends Plugin {
+module.exports = class RDLinks extends Plugin {
   async startPlugin () {
-    await this._patchWebSocketServer();
-  }
-
-  pluginWillUnload () {
-    uninject('pc-ws-onConnect');
-  }
-
-  async _patchWebSocketServer () {
-    const SocketsHandler = await getModule(m => m.Z?.__proto__?.handleConnection, false).Z;
-    console.log(SocketsHandler);
-
-    inject('pc-ws-onConnect', SocketsHandler, 'handleConnection', (args) => {
-      console.log('[WSLinks] validateSocketClient');
-      const socket = args[0];
-      console.log(socket);
-      if (socket.upgradeReq().url !== '/replugged') {
-        return args;
-      }
-      socket.close();
-      console.log(args);
-      return args;
-    }, true);
+    console.log('RDLinks Started');
   }
 };
