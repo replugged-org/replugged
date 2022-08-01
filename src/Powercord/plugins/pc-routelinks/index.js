a2a1fdfe2fda8d53d22dc75085fd23b5881132eb
Deepfried Chips <33133709+Deepfried-Chips@users.noreply.github.com>
2022-08-01 23:18:44 +0300

    plugin install links!

--- src/Powercord/plugins/pc-routelinks/index.js
+++ src/Powercord/plugins/pc-routelinks/index.js
@@ -1,7 +1,55 @@
 const { Plugin } = require('powercord/entities');
+const { getRepoInfo, cloneRepo } = require('../pc-moduleManager/util');
 const express = require('express');
+const { open: openModal, close: closeModal } = require('powercord/modal');
+const { React, getModule } = require('powercord/webpack');
+const Modal = require('../pc-moduleManager/components/ConfirmModal');
 const app = express();
 let httpserv;
+
+async function openInstallModal (info) {
+  const BOT_AVATARS = await getModule([ 'BOT_AVATARS' ]);
+  const showNotification = await getModule([ 'showNotification' ]);
+
+  showNotification.showNotification(BOT_AVATARS.replugged, 'Replugged', `Attention required with ${info.type} install prompt.`, { onClick: () => {
+    focus();
+  } }, {});
+
+  openModal(() => React.createElement(Modal, {
+    red: true,
+    header: `Install ${info.type}`,
+    desc: `Are you sure you want to install the ${info.type} ${info.repoName}?`,
+    onConfirm: () => {
+      cloneRepo(info.url, powercord, info.type);
+
+      powercord.api.notices.sendToast(`PDPluginInstalling-${info.repoName}`, {
+        header: `Installing ${info.repoName}...`,
+        type: 'info',
+        timeout: 10e3,
+        buttons: [ {
+          text: 'Got It',
+          color: 'green',
+          size: 'medium',
+          look: 'outlined'
+        } ]
+      });
+    },
+    onCancel: () => {
+      closeModal();
+      powercord.api.notices.sendToast(`PDPluginInstallCancelled-${info.repoName}`, {
+        header: `Cancelled ${info.repoName} installation`,
+        type: 'info',
+        timeout: 10e3,
+        buttons: [ {
+          text: 'Got It',
+          color: 'green',
+          size: 'medium',
+          look: 'outlined'
+        } ]
+      });
+    }
+  }));
+}
 
 module.exports = class RDLinks extends Plugin {
   async startPlugin () {
@@ -9,10 +57,13 @@ module.exports = class RDLinks extends Plugin {
       res.send('You are not supposed to be here.');
     });
 
-    app.get('/install/:what', (req, res) => {
+    app.get('/install/', (req, res) => {
       console.log(req.query);
-      if (req.params.what === 'plugin') {
-        res.send(`Sent prompt to install ${req.params.what} from ${req.query.address}`);
+      res.send(`Sent prompt to install ${req.query.address}`);
+      const info = getRepoInfo(req.query.address);
+      if (info) {
+        info.url = req.query.address;
+        openInstallModal(info);
       }
     });
 
