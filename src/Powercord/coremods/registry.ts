a3e6e856d7b66c930800edf5e1d2ecba2a0e2988
Lleyton Gray <lleyton@fyralabs.com>
2022-08-04 22:18:42 -0700

    oops

--- src/Powercord/coremods/registry.ts
+++ src/Powercord/coremods/registry.ts
@@ -1,9 +1,8 @@
 export default [
-  require('./no-track'),
-  require('./router'),
-  require('./store'),
-  require('./badges'),
-  require('./utility-classes'),
-  require('./react-devtools')
+  require("./no-track"),
+  require("./router"),
+  require("./store"),
+  require("./badges"),
+  require("./utility-classes"),
+  require("./react-devtools"),
 ];
->>>>>>> 8b36c931c6cb5dd2befb6f6c051fd4a71516f5a7:src/Powercord/coremods/registry.js
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,18 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: {
-    injector: './injectors/index.ts',
-    patcher: './src/patcher.js',
-    preload: './src/preload.js',
-    preloadSplash: './src/preloadSplash.js',
-    style: './src/Powercord/managers/style.css',
-    serialize: './src/fake_node_modules/powercord/webpack/serialize.js',
-    proxy: './src/fake_node_modules/powercord/webpack/proxy.js',
+    injector: "./injectors/index.ts",
+    patcher: "./src/patcher.js",
+    preload: "./src/preload.js",
+    preloadSplash: "./src/preloadSplash.ts",
+    style: "./src/Powercord/managers/style.css",
   },
-  outDir: 'dist',
+  outDir: "dist",
   clean: true,
-  external: [ 'electron'],
-  noExternal: [ 'fix-path' ],
+  external: ["electron"],
+  noExternal: ["fix-path"],
   loader: {
-    '.pem': 'file'
-  }
+    ".pem": "file",
+  },
 });
