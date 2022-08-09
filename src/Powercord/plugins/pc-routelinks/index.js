c012c26987d92924a7a1e231286f717465336a32
Justice Almanzar <superdash993@gmail.com>
2022-08-09 17:38:53 -0400

    use discord rpc rather than express server for installer (#70)
    * use discord rpc rather than express server
    
    * lint
    
    * bug fixes

--- src/Powercord/plugins/pc-routelinks/index.js
+++ src/Powercord/plugins/pc-routelinks/index.js
@@ -1,124 +1,95 @@
 const { Plugin } = require('powercord/entities');
 const { getRepoInfo, cloneRepo, INSTALLER_URL_REGEX } = require('../pc-moduleManager/util');
-const express = require('express');
 const { open: openModal, close: closeModal } = require('powercord/modal');
 const { getModule, React } = require('powercord/webpack');
 const Modal = require('./components/ConfirmModal');
 const { inject, uninject } = require('powercord/injector');
 
 const Anchor = getModule(m => m.default?.displayName === 'Anchor', false);
+const RPC = getModule([ 'setCommandHandler' ], false);
+const { default: RPCError } = getModule(m => m?.default?.prototype?.constructor && m.default?.toString?.().includes('RPCError'), false);
+const Socket = getModule([ 'validateSocketClient', 'getVoiceSettings' ], false);
+const { RPCErrors } = getModule([ 'RPCErrors' ], false);
 
 module.exports = class RDLinks extends Plugin {
-  async handleRequest (address) {
-    this.info = await getRepoInfo(address);
-    if (this.info) {
-      if (this.info.isInstalled) {
-        return ({
-          code: 'ALREADY-INSTALLED',
-          info: this.info
-        });
-      }
-
-      return ({
-        code: 'SUCCESS',
-        info: this.info
-      });
-    }
-    return ({
-      code: 'CANNOT-FIND'
-    });
-  }
-
   async startPlugin () {
-    this.app = express();
+    const backendURL = powercord.settings.get('backendURL');
 
-    this.app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'https://replugged.dev');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      res.header('Access-Control-Allow-Methods', 'POST');
-      next();
-    });
-
-    this.app.post('/install/', async (req, res) => {
-      const data = await this.handleRequest(req.query.address);
-      this.info = data.info;
-      if (data.code === 'SUCCESS') {
-        this.openInstallModal();
+    inject('installer-rpc-validator', Socket, 'validateSocketClient', (args, res) => {
+      const [ socket, origin ] = args;
+      if (backendURL && origin !== backendURL) {
+        return res;
       }
-      res.status(200).json(data);
+
+      socket.authorization.scopes.push('PC_BACKEND');
+
+      res.catch(error => void error);
+      return Promise.resolve();
     });
 
-    const min_port = 6473;
-    const max_port = 6480;
+    RPC.commands.PC_INSTALL = {
+      scope: 'PC_BACKEND',
+      handler: async (e) => {
+        const { address } = e.args ?? {};
+        const info = await getRepoInfo(address);
 
-    const open = async (port) => new Promise((resolve) => {
-      const server = this.app.listen(port, 'localhost', () => {
-        this.log(`Listening on port ${port}`);
-        resolve(server);
-      });
-
-      server.on('error', (e) => {
-        if (e.code === 'EADDRINUSE') {
-          this.warn(`Port ${port} is already in use.`);
-          if (port >= max_port) {
-            this.error('All ports in range are busy, cannot start server.');
-          }
-          this.log(`Trying port ${port + 1}...`);
-          resolve(open(++port));
+        if (!info) {
+          throw new RPCError(RPCErrors.INVALID_PAYLOAD, 'Could not find repository');
         }
-      });
-    });
-
-    this.httpserv = await open(min_port);
-
+        if (!info.isInstalled) {
+          this.openInstallModal(info);
+        }
+        return info;
+      }
+    };
 
     inject('installer-open-in-app', Anchor, 'default', (_, res) => {
       const link = res.props?.href?.toLowerCase();
 
-      if (link) {
-        const match = INSTALLER_URL_REGEX.exec(link);
-        if (match) {
-          // Cache info so it's loaded when you click the link
-          getRepoInfo(link);
-
-          res.props.onClick = (e) => {
-            e.preventDefault();
-            this.handleRequest(link).then(data => {
-              this.info = data.info;
-              if (data.code === 'SUCCESS') {
-                this.openInstallModal();
-              }
-              if (data.code === 'ALREADY-INSTALLED') {
-                powercord.api.notices.sendToast(`PDAlreadyInstalled-${this.info.repoName}`, {
-                  header: 'Plugin Already Installed',
-                  content: `${this.info.repoName} is already installed.`,
-                  type: 'info',
-                  timeout: 10e3,
-                  buttons: [ {
-                    text: 'Got It',
-                    color: 'green',
-                    size: 'medium',
-                    look: 'outlined'
-                  } ]
-                });
-              }
-              if (data.code === 'CANNOT-FIND') {
-                powercord.api.notices.sendToast(`PDPluginCannotFind-${link}`, {
-                  header: `Could not find a plugin or theme repository at ${link}`,
-                  type: 'info',
-                  timeout: 10e3,
-                  buttons: [ {
-                    text: 'Got It',
-                    color: 'green',
-                    size: 'medium',
-                    look: 'outlined'
-                  } ]
-                });
-              }
-            });
-          };
-        }
+      if (!link) {
+        return res;
       }
+      if (!INSTALLER_URL_REGEX.exec(link)) {
+        return res;
+      }
+
+      // Cache info so it's loaded when you click the link
+      const repoInfo = getRepoInfo(link);
+
+      res.props.onClick = (e) => {
+        e.preventDefault();
+        repoInfo.then(info => {
+          if (info) {
+            powercord.api.notices.sendToast(`PDPluginCannotFind-${link}`, {
+              header: `Could not find a plugin or theme repository at ${link}`,
+              type: 'info',
+              timeout: 10e3,
+              buttons: [ {
+                text: 'Got It',
+                color: 'green',
+                size: 'medium',
+                look: 'outlined'
+              } ]
+            });
+          } else if (info.isInstalled) {
+            powercord.api.notices.sendToast(`PDAlreadyInstalled-${info.repoName}`, {
+              header: 'Plugin Already Installed',
+              content: `${info.repoName} is already installed.`,
+              type: 'info',
+              timeout: 10e3,
+              buttons: [ {
+                text: 'Got It',
+                color: 'green',
+                size: 'medium',
+                look: 'outlined'
+              } ]
+            });
+          } else {
+            this.openInstallModal(info);
+          }
+          return info;
+        });
+      };
 
       return res;
     });
@@ -126,19 +97,19 @@ module.exports = class RDLinks extends Plugin {
     Anchor.default.displayName = 'Anchor';
   }
 
-  openInstallModal () {
+  openInstallModal (info) {
     global.DiscordNative.window.focus();
 
     openModal(() => React.createElement(Modal, {
-      type: this.info.type,
-      repoName: this.info.repoName,
-      url: this.info.url,
-      branch: this.info.branch,
+      type: info.type,
+      repoName: info.repoName,
+      url: info.url,
+      branch: info.branch,
       onConfirm: () => {
-        cloneRepo(this.info.url, powercord, this.info.type);
+        cloneRepo(info.url, powercord, info.type);
 
-        powercord.api.notices.sendToast(`PDPluginInstalling-${this.info.repoName}`, {
-          header: `Installing ${this.info.repoName}...`,
+        powercord.api.notices.sendToast(`PDPluginInstalling-${info.repoName}`, {
+          header: `Installing ${info.repoName}...`,
           type: 'info',
           timeout: 10e3,
           buttons: [ {
@@ -151,8 +122,8 @@ module.exports = class RDLinks extends Plugin {
       },
       onCancel: () => {
         closeModal();
-        powercord.api.notices.sendToast(`PDPluginInstallCancelled-${this.info.repoName}`, {
-          header: `Cancelled ${this.info.repoName} installation`,
+        powercord.api.notices.sendToast(`PDPluginInstallCancelled-${info.repoName}`, {
+          header: `Cancelled ${info.repoName} installation`,
           type: 'info',
           timeout: 10e3,
           buttons: [ {
@@ -167,7 +138,8 @@ module.exports = class RDLinks extends Plugin {
   }
 
   async pluginWillUnload () {
-    this.httpserv.close();
     uninject('installer-open-in-app');
+    uninject('installer-rpc-validator');
+    delete RPC.commands.PC_INSTALL;
   }
 };
