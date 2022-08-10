28bddb5d99635c22d6d26b0f4a36b0d25b398b2d
Vap0r1ze <superdash993@gmail.com>
2022-08-10 00:28:12 -0400

    add missing exports

--- src/Powercord/plugins/pc-moduleManager/util/index.js
+++ src/Powercord/plugins/pc-moduleManager/util/index.js
@@ -1,12 +1,12 @@
 const getRepoInfo = require('./getInfo');
 const cloneRepo = require('./cloneRepo');
-const { resp, isInstallerURL, REPO_URL_REGEX } = require('./misc');
-
+const { resp, matchRepoURL, isInstallerURL, REPO_URL_REGEX } = require('./misc');
 
 module.exports = {
   getRepoInfo,
   cloneRepo,
   resp,
+  matchRepoURL,
   isInstallerURL,
   REPO_URL_REGEX
 };
