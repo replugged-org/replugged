7e7eac983b6edf3fa1ec8a372e7d0781b51b9d66
Vap0r1ze <superdash993@gmail.com>
2022-08-11 15:38:15 -0400

    lint

--- src/Powercord/plugins/pc-moduleManager/index.js
+++ src/Powercord/plugins/pc-moduleManager/index.js
@@ -278,11 +278,15 @@ module.exports = class ModuleManager extends Plugin {
   async _applyQuickCSS (css, save = false) {
     this._quickCSS = css.trim();
     this._quickCSSElement.innerHTML = this._quickCSS;
-    await this._saveQuickCSS(null, false);
+    if (save) {
+      await this._saveQuickCSS(null, false);
+    }
   }
 
   async _saveQuickCSS (css, apply = true) {
-    if (apply) await this._applyQuickCSS(css, false);
+    if (apply) {
+      await this._applyQuickCSS(css, false);
+    }
     await writeFile(this._quickCSSFile, this._quickCSS);
   }
 
