3f28ea02f8d1ae596ebb1d0cfc2aa67e9e17b6a2
spin~ <public@spin.rip>
2022-08-12 11:39:45 -0700

    Fix "React not defined" error
    and switch to singe-quote, we love eslint

--- src/Powercord/plugins/pc-settings/components/RepluggedLink.jsx
+++ src/Powercord/plugins/pc-settings/components/RepluggedLink.jsx
@@ -1,19 +1,19 @@
-const { getModule } = require('powercord/webpack');
+const { React, getModule } = require('powercord/webpack');
 const AsyncComponent = require('powercord/components/AsyncComponent');
 
 const RepluggedIcon = require('./RepluggedIcon');
 const Anchor = AsyncComponent.fromDisplayName('Anchor');
-const socialClassNames = getModule([ 'link', 'socialLinks' ], false);
+const socialClassNames = getModule(['link', 'socialLinks'], false);
 
 module.exports = ({ className = '', href }) => (
-  <Anchor
-    title="Replugged"
-    href={href}
-    className={`${socialClassNames.link} ${className}`}
-    rel="author"
-    tabindex="-1"
-    target="_blank"
-  >
-    <RepluggedIcon/>
-  </Anchor>
+    <Anchor
+        title='Replugged'
+        href={href}
+        className={`${socialClassNames.link} ${className}`}
+        rel='author'
+        tabindex='-1'
+        target='_blank'
+    >
+        <RepluggedIcon />
+    </Anchor>
 );
