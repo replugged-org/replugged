15b8fd09716c638d6bd28b07a38f098ab04e6a51
spin~ <spin@seized.email>
2022-08-12 14:14:24 -0700

    fix eslint issues sorry for so many commits lmao

--- src/Powercord/plugins/pc-settings/components/RepluggedLink.jsx
+++ src/Powercord/plugins/pc-settings/components/RepluggedLink.jsx
@@ -3,17 +3,17 @@ const AsyncComponent = require('powercord/components/AsyncComponent');
 
 const RepluggedIcon = require('./RepluggedIcon');
 const Anchor = AsyncComponent.fromDisplayName('Anchor');
-const socialClassNames = getModule(['link', 'socialLinks'], false);
+const socialClassNames = getModule([ 'link', 'socialLinks' ], false);
 
 module.exports = ({ className = '', href }) => (
-    <Anchor
-        title='Replugged'
-        href={href}
-        className={`${socialClassNames.link} ${className}`}
-        rel='author'
-        tabindex='-1'
-        target='_blank'
-    >
-        <RepluggedIcon />
-    </Anchor>
+  <Anchor
+    title='Replugged'
+    href={href}
+    className={`${socialClassNames.link} ${className}`}
+    rel='author'
+    tabindex='-1'
+    target='_blank'
+  >
+    <RepluggedIcon />
+  </Anchor>
 );
