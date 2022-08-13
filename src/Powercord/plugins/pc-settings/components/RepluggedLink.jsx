733212aa683fd61acdc5a92e4738ee5f7fa4593d
spin~ <public@spin.rip>
2022-08-12 23:10:29 -0700

    Update branding in Discord settings & validate plugin/theme invites (#87)
    * Update branding in Discord settings
    
    * Make the image a bit smaller
    
    * Add check for Discord invite
    
    * Change to single-quote
    
    * Revert branding until SVG can be implemented
    
    * svg replugged social link
    
    * Fix "React not defined" error
    
    and switch to singe-quote, we love eslint
    
    * Fix "React not defined" error
    
    and switch to singe-quote, we love eslint
    
    * Update style.scss
    
    * eslint my beloved
    
    * fix eslint issues
    sorry for so many commits lmao
    
    Co-authored-by: Vap0r1ze <superdash993@gmail.com>

---
+++ src/Powercord/plugins/pc-settings/components/RepluggedLink.jsx
@@ -0,0 +1,19 @@
+const { React, getModule } = require('powercord/webpack');
+const AsyncComponent = require('powercord/components/AsyncComponent');
+
+const RepluggedIcon = require('./RepluggedIcon');
+const Anchor = AsyncComponent.fromDisplayName('Anchor');
+const socialClassNames = getModule([ 'link', 'socialLinks' ], false);
+
+module.exports = ({ className = '', href }) => (
+  <Anchor
+    title='Replugged'
+    href={href}
+    className={`${socialClassNames.link} ${className}`}
+    rel='author'
+    tabindex='-1'
+    target='_blank'
+  >
+    <RepluggedIcon />
+  </Anchor>
+);
