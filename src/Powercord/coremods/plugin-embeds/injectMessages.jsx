8121126f239ef40ef103531a110a130025e3bbad
Albert Portnoy <albert@albertp.dev>
2022-08-21 12:19:06 -0500

    Change how it works

--- src/Powercord/coremods/plugin-embeds/injectMessages.jsx
+++ src/Powercord/coremods/plugin-embeds/injectMessages.jsx
@@ -1,32 +1,30 @@
 const { getModule, React } = require('powercord/webpack');
 const PluginEmbed = require('./components/PluginEmbed');
 
+const { matchRepoURL } = require('../moduleManager/util');
+
 const SimpleMarkdown = getModule([ 'defaultRules', 'astParserFor' ], false);
-const regex = /^https?:\/\/(?:www\.)?github\.com\/([^/\s>]+)\/([^/\s>]+)(?:\/tree\/([^\s>]+))?/;
+
+let originalFn;
 
 module.exports.injectMessages = function () {
-  SimpleMarkdown.defaultRules.pluginLink = {
-    order: SimpleMarkdown.defaultRules.url.order - 0.5,
-    match (source) {
-      return regex.exec(source);
-    },
-    parse (capture) {
-      if (!capture.input.endsWith('/')) {
-        capture.input += '/';
-      }
-      return {
-        match: capture,
-        url: capture.input
-      };
-    },
-    react (node) {
+  originalFn = SimpleMarkdown.defaultRules.link.react;
+  SimpleMarkdown.defaultRules.link.react = (...args) => {
+    const res = originalFn(...args);
+
+    const match = matchRepoURL(res.props.href);
+    if (match) {
       return (
-        <PluginEmbed url={node.url} match={node.match} />
+        <PluginEmbed match={match} />
       );
     }
+
+    return res;
   };
 };
 
 module.exports.uninjectMessages = function () {
-  delete SimpleMarkdown.defaultRules.pluginLink;
+  if (originalFn) {
+    SimpleMarkdown.defaultRules.link.react = originalFn;
+  }
 };
