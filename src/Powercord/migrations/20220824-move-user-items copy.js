36d400f8a7ef329e9c03678e2fe5d5a2a956dafe
DracoClaw <34274162+DracoClaw@users.noreply.github.com>
2022-08-24 20:28:30 +0300

    migration script

---
+++ src/Powercord/migrations/20220824-move-user-items copy.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const { join } = require('path');
+
+module.exports = () => {
+  const oldPlugs = join(__dirname, '..', '..', '..', 'plugins');
+  const newPlugs = join(__dirname, '..', '..', '..', 'user', 'plugins');
+  const oldThemes = join(__dirname, '..', '..', '..', 'themes');
+  const newThemes = join(__dirname, '..', '..', '..', 'user', 'themes');
+  const oldSettings = join(__dirname, '..', '..', '..', 'settings');
+  const newSettings = join(__dirname, '..', '..', '..', 'user', 'settings');
+
+
+  // This function recursively moves all subdirectories and files from src to dest
+  let moveRecursiveSync = function(src, dest) {
+    fs.readdirSync(src).forEach((name) => {
+      fs.renameSync(join(src, name), join(dest, name));
+    });
+  };
+
+  if (fs.existsSync(oldPlugs)) {
+    moveRecursiveSync(oldPlugs, newPlugs);
+    fs.rmdirSync(oldPlugs);
+  }
+  if (fs.existsSync(oldThemes)) {
+    moveRecursiveSync(oldThemes, newThemes);
+    fs.rmdirSync(oldThemes);
+  }
+  if (fs.existsSync(oldSettings)) {
+    moveRecursiveSync(oldSettings, newSettings);
+    fs.rmdirSync(oldSettings);
+  }
+};
