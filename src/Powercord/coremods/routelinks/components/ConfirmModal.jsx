d61e48a742f85f5ae78cdc2d90fafb9f6ee4fc02
Abstract <36399055+Abstructor@users.noreply.github.com>
2022-09-01 10:50:37 +0700

    Fix crash with .install command && the install modal messing up and 1 last string I missed (#175)
    * Make some strings unhardcoded, thus allowing them to be translatable via i18n
    
    * Make more strings unhardcoded
    
    * Fix quirks in i18n tags
    
    * Unhardcode more strings
    
    * Unhardcode more strings
    
    * Fix problems caused by dementia
    
    * I hate linters
    
    * Fix crash with .install command && the install modal messing up

--- src/Powercord/coremods/routelinks/components/ConfirmModal.jsx
+++ src/Powercord/coremods/routelinks/components/ConfirmModal.jsx
@@ -19,17 +19,17 @@ module.exports = class Modal extends React.Component {
 
     return <Confirm
       red={true}
-      header={`Install ${this.props.type}`}
+      header={Messages.REPLUGGED_INSTALL_MODAL_HEADER.format({ name: this.props.repoName })}
       confirmText={Messages.REPLUGGED_CONFIRM}
       cancelText={Messages.REPLUGGED_CANCEL}
       onConfirm={() => this.props.onConfirm()}
       onCancel={() => typeof this.props.onCancel !== 'undefined' ? this.props.onCancel() : closeModal()}
-    > //Are you sure you want to install the {} {} from ?
+    >
       <Text> {Messages.REPLUGGED_MODULE_MANAGER_CONFIRM_INSTALL.format({
         type: this.props.type,
         name: this.props.repoName,
-        url: `<a href={this.props.url} target="_blank">${this.props.url}</a>`,
-        branch: this.props.branch ? `(${this.props.branch} branch)`: ''
+        url: <a href={this.props.url} target="_blank">{this.props.url}</a>,
+        branch: this.props.branch ? Messages.REPLUGGED_INSTALL_MODAL_BRANCH.format({ branch: this.props.branch }) : ''
       })} </Text>
     </Confirm>;
   }
