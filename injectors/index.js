e5c91eab7a2ca31d438fdf4200219cce7a0fcfeb
Ryan Omasta <ryand@emeraldsys.xyz>
2022-09-03 14:23:14 -0600

    Fix

--- injectors/index.js
+++ injectors/index.js
@@ -27,17 +27,13 @@ try {
   }
 }
 
-let platform = (process.argv[4] || 'stable').toLowerCase();
-
-if (!main.VALID_PLATFORMS.includes(platform)) {
-	console.log(`${AnsiEscapes.RED}Platform you specified isn't valid, please specify a valid one.${AnsiEscapes.RESET}\n\nList of valid platforms:\n${AnsiEscapes.GREEN}${main.VALID_PLATFORMS.map(x => `${x}`).join('\n')}${AnsiEscapes.RESET}`);
-	process.exit(process.argv.includes('--no-exit-codes') ? 0 : 1);
-}
+let platform = process.argv[4]?.toLowerCase();
 
 (async () => {
   const exists = main.checkPlatform(platform);
 
   if (!exists) {
+    platform = 'stable';
     console.log(`${AnsiEscapes.YELLOW}No valid platform specified, defaulting to "${platform}".${AnsiEscapes.RESET}\n`);
   } else if (platform === 'development') {
     platform = 'dev';
