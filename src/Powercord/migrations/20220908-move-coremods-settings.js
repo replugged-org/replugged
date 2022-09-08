02ee545d036437b9eeb78fd3c5071dfcb178fcfc
DracoClaw <34274162+DracoClaw@users.noreply.github.com>
2022-09-08 16:56:38 +0300

    migrate non-core mods

--- src/Powercord/migrations/20220908-move-coremods-settings.js
+++ src/Powercord/migrations/20220908-move-coremods-settings.js
@@ -21,5 +21,39 @@ module.exports = () => {
 
   // Check formerly built in plugins.
 
+  if (existsSync(`${__dirname}/../../../settings/pc-clickableEdits.json`)) {
+    renameSync(`${__dirname}/../../../settings/pc-clickableEdits.json`, `${__dirname}/../../../settings/clickableEdits.json`);
+  }
+
+  if (existsSync(`${__dirname}/../../../settings/pc-emojiUtility.json`)) {
+    renameSync(`${__dirname}/../../../settings/pc-emojiUtility.json`, `${__dirname}/../../../settings/emojiUtility.json`);
+  }
   
+  if (existsSync(`${__dirname}/../../../settings/pc-heygirl.json`)) {
+    renameSync(`${__dirname}/../../../settings/pc-heygirl.json`, `${__dirname}/../../../settings/heygirl.json`);
+  }
+
+  if (existsSync(`${__dirname}/../../../settings/pc-clickableEdits.json`)) {
+    renameSync(`${__dirname}/../../../settings/pc-clickableEdits.json`, `${__dirname}/../../../settings/clickableEdits.json`);
+  }
+
+  if (existsSync(`${__dirname}/../../../settings/pc-codeblocks.json`)) {
+    renameSync(`${__dirname}/../../../settings/pc-codeblocks.json`, `${__dirname}/../../../settings/better-codeblocks.json`);
+  }
+  
+  if (existsSync(`${__dirname}/../../../settings/pc-lmgtfy.json`)) {
+    renameSync(`${__dirname}/../../../settings/pc-lmgtfy.json`, `${__dirname}/../../../settings/lmgtfy.json`);
+  }
+
+  if (existsSync(`${__dirname}/../../../settings/pc-mock.json`)) {
+    renameSync(`${__dirname}/../../../settings/pc-mock.json`, `${__dirname}/../../../settings/mock.json`);
+  }
+
+  if (existsSync(`${__dirname}/../../../settings/pc-tags.json`)) {
+    renameSync(`${__dirname}/../../../settings/pc-tags.json`, `${__dirname}/../../../settings/tags.json`);
+  }
+  
+  if (existsSync(`${__dirname}/../../../settings/pc-spotify.json`)) {
+    renameSync(`${__dirname}/../../../settings/pc-spotify.json`, `${__dirname}/../../../settings/spotify-modal.json`);
+  }
 };
