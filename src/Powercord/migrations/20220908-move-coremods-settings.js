3a756f963f14bf7b846507718c9f3e7514a7a0ed
DracoClaw <34274162+DracoClaw@users.noreply.github.com>
2022-09-09 17:04:07 +0300

    migration loop

--- src/Powercord/migrations/20220908-move-coremods-settings.js
+++ src/Powercord/migrations/20220908-move-coremods-settings.js
@@ -1,59 +1,30 @@
-const { existsSync, renameSync } = require('fs');
+const { existsSync, renameSync, unlinkSync } = require('fs');
+const path = require('path');
 
 module.exports = () => {
-  // Check if coremod settings exist.
 
-  if (existsSync(`${__dirname}/../../../settings/pc-general.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-general.json`, `${__dirname}/../../../settings/coremods/general.json`);
-  }
+  const settingsPath = `${__dirname}/../../../settings`;
+  const files = {
+    "pc-general.json": "coremods/general.json",
+    "pc-moduleManager.json": "coremods/moduleManager.json",
+    "pc-updater.json": "coremods/updater.json",
+    "rp-migrations.json": "coremods/migrations.json",
+    "pc-clickableEdits.json": "clickableEdits.json",
+    "pc-emojiUtility.json": "emojiUtility.json",
+    "pc-heygirl.json": "heygirl.json",
+    "pc-hastebin.json": "hastebin.json",
+    "pc-codeblocks.json": "better-codeblocks.json",
+    "pc-lmgtfy.json": "lmgtfy.json",
+    "pc-mock.json": "mock.json",
+    "pc-tags.json": "tags.json",
+    "pc-spotify.json": "spotify-modal.json"
+  };
 
-  if (existsSync(`${__dirname}/../../../settings/pc-moduleManager.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-moduleManager.json`, `${__dirname}/../../../settings/coremods/moduleManager.json`);
-  }
-
-  if (existsSync(`${__dirname}/../../../settings/pc-updater.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-updater.json`, `${__dirname}/../../../settings/coremods/updater.json`);
-  }
-
-  if (existsSync(`${__dirname}/../../../settings/rp-migrations.json`)) {
-    renameSync(`${__dirname}/../../../settings/rp-migrations.json`, `${__dirname}/../../../settings/coremods/migrations.json`);
-  }
-
-  // Check formerly built in plugins.
-
-  if (existsSync(`${__dirname}/../../../settings/pc-clickableEdits.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-clickableEdits.json`, `${__dirname}/../../../settings/clickableEdits.json`);
-  }
-
-  if (existsSync(`${__dirname}/../../../settings/pc-emojiUtility.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-emojiUtility.json`, `${__dirname}/../../../settings/emojiUtility.json`);
-  }
-
-  if (existsSync(`${__dirname}/../../../settings/pc-heygirl.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-heygirl.json`, `${__dirname}/../../../settings/heygirl.json`);
-  }
-
-  if (existsSync(`${__dirname}/../../../settings/pc-clickableEdits.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-clickableEdits.json`, `${__dirname}/../../../settings/clickableEdits.json`);
-  }
-
-  if (existsSync(`${__dirname}/../../../settings/pc-codeblocks.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-codeblocks.json`, `${__dirname}/../../../settings/better-codeblocks.json`);
-  }
-
-  if (existsSync(`${__dirname}/../../../settings/pc-lmgtfy.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-lmgtfy.json`, `${__dirname}/../../../settings/lmgtfy.json`);
-  }
-
-  if (existsSync(`${__dirname}/../../../settings/pc-mock.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-mock.json`, `${__dirname}/../../../settings/mock.json`);
-  }
-
-  if (existsSync(`${__dirname}/../../../settings/pc-tags.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-tags.json`, `${__dirname}/../../../settings/tags.json`);
-  }
-
-  if (existsSync(`${__dirname}/../../../settings/pc-spotify.json`)) {
-    renameSync(`${__dirname}/../../../settings/pc-spotify.json`, `${__dirname}/../../../settings/spotify-modal.json`);
+  for (const setting in files) {
+    if (existsSync(`${settingsPath}/${setting}`)) {
+      if (!existsSync(`${settingsPath}/${files[setting]}`)) {
+        renameSync(`${settingsPath}/${setting}`, `${settingsPath}/${files[setting]}`);
+      }
+    }
   }
 };
