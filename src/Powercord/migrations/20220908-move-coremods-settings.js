0b21512d0388acb5b8528c934d13893a267fb785
DracoClaw <34274162+DracoClaw@users.noreply.github.com>
2022-09-09 17:18:36 +0300

    cleanup loop

--- src/Powercord/migrations/20220908-move-coremods-settings.js
+++ src/Powercord/migrations/20220908-move-coremods-settings.js
@@ -5,25 +5,31 @@ module.exports = () => {
 
   const settingsPath = `${__dirname}/../../../settings`;
   const files = {
-    "pc-general.json": "coremods/general.json",
-    "pc-moduleManager.json": "coremods/moduleManager.json",
-    "pc-updater.json": "coremods/updater.json",
-    "rp-migrations.json": "coremods/migrations.json",
-    "pc-clickableEdits.json": "clickableEdits.json",
-    "pc-emojiUtility.json": "emojiUtility.json",
-    "pc-heygirl.json": "heygirl.json",
-    "pc-hastebin.json": "hastebin.json",
-    "pc-codeblocks.json": "better-codeblocks.json",
-    "pc-lmgtfy.json": "lmgtfy.json",
-    "pc-mock.json": "mock.json",
-    "pc-tags.json": "tags.json",
-    "pc-spotify.json": "spotify-modal.json"
+    'pc-general.json': 'coremods/general.json',
+    'pc-moduleManager.json': 'coremods/moduleManager.json',
+    'pc-updater.json': 'coremods/updater.json',
+    'rp-migrations.json': 'coremods/migrations.json',
+    'pc-clickableEdits.json': 'clickableEdits.json',
+    'pc-emojiUtility.json': 'emojiUtility.json',
+    'pc-heygirl.json': 'heygirl.json',
+    'pc-hastebin.json': 'hastebin.json',
+    'pc-codeblocks.json': 'better-codeblocks.json',
+    'pc-lmgtfy.json': 'lmgtfy.json',
+    'pc-mock.json': 'mock.json',
+    'pc-tags.json': 'tags.json',
+    'pc-spotify.json': 'spotify-modal.json'
   };
 
   for (const setting in files) {
-    if (existsSync(`${settingsPath}/${setting}`)) {
-      if (!existsSync(`${settingsPath}/${files[setting]}`)) {
-        renameSync(`${settingsPath}/${setting}`, `${settingsPath}/${files[setting]}`);
+    const source = path.resolve(`${settingsPath}/${setting}`);
+    const target = path.resolve(`${settingsPath}/${files[setting]}`);
+  
+    if (existsSync(source)) {
+      if (!existsSync(target)) {
+        renameSync(source, target);
+        if (existsSync(source)) {
+          unlinkSync(source);
+        }
       }
     }
   }
