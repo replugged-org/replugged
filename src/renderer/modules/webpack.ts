a78c88a57bcec23a56e5922f6559d293cee8a41d
Albert Portnoy <albert@albertp.dev>
2022-09-30 22:05:16 -0500

    Better dev experience

--- src/renderer/modules/webpack.ts
+++ src/renderer/modules/webpack.ts
@@ -1,4 +1,4 @@
-function getAllExportsForModule (module: Module): (Record<string, unknown> | null)[] {
+function getAllExportsForModule (module: RawModule): (Record<string, unknown> | null)[] {
   const { exports } = module;
   if (!exports || typeof exports !== 'object') {
     return [];
@@ -13,19 +13,51 @@ export const filters = {
       props = [ props ];
     }
 
-    return (m: Module) => {
-      const keysInModule = getAllExportsForModule(m).flatMap(x => x && Object.keys(x)).filter(Boolean);
-      return (props as string[]).every(prop => keysInModule.includes(prop));
+    return (m: RawModule) => {
+      const exports = getAllExportsForModule(m);
+      return exports.find(x => x && (props as string[]).every(prop => Object.keys(x).includes(prop)));
     };
   }
   // byString: () => {}
 };
 
-export type Module = Record<string, unknown> & {
+type Exports = Record<string, unknown> | ((...args: unknown[]) => unknown) | string;
+
+export type RawModule = Record<string, unknown> & {
   id: number;
   loaded: boolean;
-  exports: Record<string, unknown> | ((...args: unknown[]) => unknown) | string;
+  exports: Exports
 };
+
+class Module {
+  public id: number;
+  public exports: Exports;
+  public loaded: boolean;
+
+  constructor (module: RawModule) {
+    Object.assign(this, module.props);
+
+    this.id = module.id;
+    this.exports = module.exports;
+    this.loaded = module.loaded;
+  }
+
+  public findExportForProps (props: string | string[]): Record<string, unknown> | null {
+    if (!Array.isArray(props)) {
+      props = [ props ];
+    }
+
+    if (!this.exports || typeof this.exports !== 'object') {
+      return null;
+    }
+
+    const objectExports = Object.values(this.exports).filter(x => typeof x === 'object') as Record<string, unknown>[];
+
+    return objectExports.find(x => (props as string[]).every(prop => Object.keys(x).includes(prop))) ?? null;
+  }
+}
+
+type ModuleType = typeof Module & Record<string, unknown>;
 
 // @todo Probably want to store this elsewhere
 if (!window.wpCache) {
@@ -52,18 +84,34 @@ export async function loadWebpackModules () {
   ]));
 }
 
-type FilterFunction = (module: Module) => boolean;
+type FilterFunction = (module: RawModule) => boolean | Exports;
 type Filter = string | FilterFunction;
 
-export function getModule (filter: Filter | Filter[], all: true): Module[];
-export function getModule (filter: Filter | Filter[], all: false | undefined): Module | null;
-export function getModule (filter: Filter | Filter[], all = false): Module | Module[] | null {
+export function getModule (filter: Filter | Filter[], all: true): ModuleType[];
+export function getModule (filter: Filter | Filter[], all: false | undefined): ModuleType | null;
+export function getModule (filter: Filter | Filter[], all = false): ModuleType | ModuleType[] | null {
   if (!Array.isArray(filter)) {
     filter = [ filter ];
   }
   const filterFunctionArray = filter.map(f => typeof f === 'string' ? filters.byProps(f) : f);
 
-  const matchingModules = window.wpCache.filter(m => filterFunctionArray.every(f => f(m)));
+  const matchingModules = window.wpCache
+    .map(m => {
+      const result = filterFunctionArray.map(f => f(m)).filter(Boolean);
+      if (!result.length) {
+        return;
+      }
+      const isAllObjects = result.every(x => typeof x === 'object');
+      if (isAllObjects) {
+        m.props = {};
+        result.forEach(x => Object.assign(m.props, x));
+      } else {
+        m.props = m.exports;
+      }
+
+      return new Module(m);
+    })
+    .filter(Boolean) as unknown as ModuleType[];
 
   if (!all) {
     return matchingModules[0] || null;
