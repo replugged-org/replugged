6ecea53a42c577fe405915dcbaa85c866c6d2eb9
12944qwerty <qwerty12944qwerty@gmail.com>
2022-10-01 21:54:13 -0500

    react devtools

--- src/main/index.ts
+++ src/main/index.ts
@@ -134,4 +134,11 @@ electron.app.once('ready', () => {
 
 require('./ipc');
 
+const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
+electron.app.whenReady().then(() => { // optionify it later with sdk
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name: string) => console.log(`Added Extension:  ${name}`))
+    .catch((err: string) => console.log('An error occurred: ', err));
+});
+
 require('module')._load(discordPath);
