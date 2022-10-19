a64310eaef48e1f38bfed85b72f18ce183466a54
Alyxia Sother <alyxia@riseup.net>
2022-10-19 23:45:10 +0200

    Add React typings

--- src/renderer/coremods/settings/index.tsx
+++ src/renderer/coremods/settings/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Coremod from "../../entities/coremod";
 import { patchPlaintext } from "../../modules/webpack";
 import { settingsTools, insertSections } from "./lib";
