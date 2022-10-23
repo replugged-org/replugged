828466ef29336dc7b7fe32a0380c213334775c06
Albert Portnoy <albert@albertp.dev>
2022-10-23 12:30:21 -0500

    Add electron

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,22 +1,24 @@
 // TODO: Scope global types to each component
 
-import { WebpackChunkGlobal } from "./types/discord";
-import * as replugged from "./renderer/replugged";
-import { RepluggedNativeType } from "./preload";
-import type React from "react";
+import {WebpackChunkGlobal} from './types/discord';
+import * as replugged from './renderer/replugged';
+import {RepluggedNativeType} from './preload';
+import type React from 'react';
 
 declare global {
-  export var appSettings: {
-    set(setting: string, v: unknown): void;
-  };
+	export var appSettings: {
+		set(setting: string, v: unknown): void;
+	};
 
-  interface Window {
-    React: typeof React;
-    RepluggedNative: RepluggedNativeType;
-    replugged: typeof replugged;
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    webpackChunkdiscord_app: WebpackChunkGlobal;
-  }
+	interface Window {
+		React: typeof React;
+		RepluggedNative: RepluggedNativeType;
+		replugged: typeof replugged;
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		webpackChunkdiscord_app: WebpackChunkGlobal;
+		// TODO: types for this
+		DiscordNative: Record<string, unknown>;
+	}
 }
 
 export {};
--- src/renderer/modules/webpack/common.ts
+++ src/renderer/modules/webpack/common.ts
@@ -1,61 +1,75 @@
-import { getByProps, getBySource, getModule } from ".";
-import { ModuleExports } from "../../../types/discord";
-import { Filter } from "../../../types/webpack";
+import {getByProps, getBySource, getModule} from '.';
+import {ModuleExports} from '../../../types/discord';
+import {Filter} from '../../../types/webpack';
 
 import type React from 'react';
+import type Electron from 'electron';
 
 enum CommonModuleFilterTypes {
-  PROPS,
-  SOURCE,
-  FUNCTION
+	PROPS,
+	SOURCE,
+	FUNCTION,
 }
 
 interface CommonModulePropsFilter {
-  type: CommonModuleFilterTypes.PROPS,
-  props: string[]
+	type: CommonModuleFilterTypes.PROPS;
+	props: string[];
 }
 
 interface CommonModuleSourceFilter {
-  type: CommonModuleFilterTypes.SOURCE,
-  match: string | RegExp
+	type: CommonModuleFilterTypes.SOURCE;
+	match: string | RegExp;
 }
 
 interface CommonModuleFunctionFilter {
-  type: CommonModuleFilterTypes.FUNCTION,
-  filter: Filter
+	type: CommonModuleFilterTypes.FUNCTION;
+	filter: Filter;
 }
 
-type CommonModuleFilter = CommonModulePropsFilter | CommonModuleSourceFilter | CommonModuleFunctionFilter;
+type CommonModuleFilter =
+	| CommonModulePropsFilter
+	| CommonModuleSourceFilter
+	| CommonModuleFunctionFilter;
 
 const commonModules = new Map<string, ModuleExports | undefined>();
 
-
-function findCommonModule(filter: CommonModuleFilter): ModuleExports | undefined {
-  switch (filter.type) {
-    case CommonModuleFilterTypes.PROPS:
-      return getByProps(...filter.props);
-    case CommonModuleFilterTypes.SOURCE:
-      return getBySource(filter.match);
-    case CommonModuleFilterTypes.FUNCTION:
-      return getModule(filter.filter);
-  }
+function findCommonModule(
+	filter: CommonModuleFilter,
+): ModuleExports | undefined {
+	switch (filter.type) {
+		case CommonModuleFilterTypes.PROPS:
+			return getByProps(...filter.props);
+		case CommonModuleFilterTypes.SOURCE:
+			return getBySource(filter.match);
+		case CommonModuleFilterTypes.FUNCTION:
+			return getModule(filter.filter);
+	}
 }
 
-function getCommonModule(name: string, filter: CommonModuleFilter): ModuleExports | undefined {
-  if (!commonModules.has(name)) {
-    const found = findCommonModule(filter);
-    if (found !== void 0) {
-      commonModules.set(name, found);
-    }
-  }
-  return commonModules.get(name);
+function getCommonModule(
+	name: string,
+	filter: CommonModuleFilter,
+): ModuleExports | undefined {
+	if (!commonModules.has(name)) {
+		const found = findCommonModule(filter);
+		if (found !== void 0) {
+			commonModules.set(name, found);
+		}
+	}
+	return commonModules.get(name);
 }
 
 export default {
-  get React(): typeof React {
-    return getCommonModule('React', {
-      type: CommonModuleFilterTypes.PROPS,
-      props: ['__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED', 'createElement']
-    }) as typeof React;
-  }
+	get React(): typeof React {
+		return getCommonModule('React', {
+			type: CommonModuleFilterTypes.PROPS,
+			props: [
+				'__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED',
+				'createElement',
+			],
+		}) as typeof React;
+	},
+	get electron(): Electron.App {
+		return window.DiscordNative.app as Electron.App;
+	},
 };
