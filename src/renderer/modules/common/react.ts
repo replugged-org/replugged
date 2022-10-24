bb53e0ae34378da9a53f2adf1376c78e6c79e427
Lleyton Gray <lleyton@fyralabs.com>
2022-10-23 21:22:03 -0700

    work better with named exports

--- src/renderer/modules/common/react.ts
+++ src/renderer/modules/common/react.ts
@@ -1,9 +1,12 @@
 import { byPropsFilter, waitFor } from "../webpack";
 
-let React: typeof import("react");
+// @ts-expect-error - webpack exports are loaded async
+let React: typeof import("react") & { reactReady: Promise<void> } = {
+  reactReady: waitFor(
+    byPropsFilter(["__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED", "createElement"]),
+  ).then((react) => {
+    Object.assign(React, react as typeof import("react"));
+  }),
+};
 
-export const reactReady = waitFor(
-  byPropsFilter(["__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED", "createElement"]),
-).then((react) => (React = react as typeof import("react")));
-
-export { React as default };
+export = React;
