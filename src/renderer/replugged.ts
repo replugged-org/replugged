20b64b16bb41cadfadb6497eb690d997b562e660
Albert Portnoy <albert@albertp.dev>
2022-12-11 21:33:38 -0600

    Mark stuff internal/hidden

--- src/renderer/replugged.ts
+++ src/renderer/replugged.ts
@@ -4,9 +4,26 @@ export * as webpack from "./modules/webpack";
 export { default as notices } from "./apis/notices";
 export { default as commands } from "./apis/commands";
 export { default as settings } from "./apis/settings";
+/**
+ * @internal
+ * @hidden
+ */
 export * as coremods from "./managers/coremods";
+/**
+ * @internal Mostly for internal use, or within the console.
+ */
 export * as quickCSS from "./managers/quick-css";
+/**
+ * @internal Mostly for internal use, or within the console.
+ */
 export * as themes from "./managers/themes";
+/**
+ * @internal
+ * @hidden
+ */
 export * as ignition from "./managers/ignition";
+/**
+ * @internal Mostly for internal use, or within the console.
+ */
 export * as plugins from "./managers/plugins";
 export * from "../types";
