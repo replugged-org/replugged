7363b009958e5582bf66f2a7a10c909d1fe84c31
Albert Portnoy <albert@albertp.dev>
2022-12-12 21:16:28 -0600

    Constants

--- src/renderer/modules/webpack-common.ts
+++ src/renderer/modules/webpack-common.ts
@@ -1,6 +1,7 @@
 import { Filter, ModuleExports, webpack } from "@replugged";
 const { filters, waitForModule } = webpack;
 import { Messages, Typing } from "src/types/webpack-common";
+import { getExportsForProps } from "./webpack";
 
 async function wrapFilter<T extends ModuleExports>(filter: Filter): Promise<T | null> {
   return (await waitForModule(filter, {
@@ -13,3 +14,29 @@ export const messages = wrapFilter<Messages>(
 );
 
 export const typing = wrapFilter<Typing>(filters.byProps("startTyping", "stopTyping"));
+
+export const constants = wrapFilter<ModuleExports>(filters.bySource("BASE_URL:")).then((mod) => {
+  if (!mod) return null;
+
+  return {
+    raw: mod,
+    Permissions: getExportsForProps(mod, ["ADMINISTRATOR", "MANAGE_GUILD"]) as Record<
+      string,
+      bigint
+    >,
+    Scopes: getExportsForProps(mod, ["BOT", "GUILDS"]) as Record<string, string>,
+    RPCErrors: getExportsForProps(mod, ["RATELIMITED", "TOKEN_REVOKED"]) as Record<
+      string | number,
+      string | number
+    >,
+    RPCCommands: getExportsForProps(mod, ["AUTHENTICATE", "AUTHORIZE"]) as Record<string, string>,
+    RPCEvents: getExportsForProps(mod, ["GUILD_CREATE", "ERROR"]) as Record<string, string>,
+    Colors: getExportsForProps(mod, ["GREY1", "GREY2"]) as Record<string, string>,
+    Status: getExportsForProps(mod, ["ONLINE", "IDLE"]) as Record<string, string>,
+    CSSVariables: getExportsForProps(mod, ["TEXT_NORMAL", "BACKGROUND_PRIMARY"]) as Record<
+      string,
+      string
+    >,
+    Paths: getExportsForProps(mod, ["INDEX", "DOWNLOADS"]) as Record<string, string>,
+  };
+});
