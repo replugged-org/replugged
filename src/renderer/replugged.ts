25fa35ccca15b255c52aa91c3a27415cd981d13e
12944qwerty <qwerty12944qwerty@gmail.com>
2022-12-14 17:05:16 -0600

    WIP

--- src/renderer/replugged.ts
+++ src/renderer/replugged.ts
@@ -1,16 +1,24 @@
 import * as webpackModule from "./modules/webpack";
 import webpackCommon, { CommonModules } from "./modules/webpack-common";
+import components_, { Components } from './modules/components';
 
 export * as injector from "./modules/injector";
 export { Injector } from "./modules/injector";
+
 export const webpack = {
   ...webpackModule,
   common: null as unknown as CommonModules,
 };
+
+export let components = null as unknown as Components;
+
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 webpackCommon().then((modules) => {
   webpack.common = modules;
 });
+components_().then((modules) => {
+  components = modules;
+})
 export { default as notices } from "./apis/notices";
 export { default as commands } from "./apis/commands";
 export { default as settings } from "./apis/settings";
