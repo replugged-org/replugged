26d770a0bd4b257dd77821da115c3aea4a23ceea
12944qwerty <qwerty12944qwerty@gmail.com>
2022-12-17 18:45:52 -0600

    Modal imports

--- src/renderer/modules/components.tsx
+++ src/renderer/modules/components.tsx
@@ -1,11 +1,11 @@
-import { filters, getBySource, waitForModule } from './webpack';
-import { ModuleExports, Filter } from '@replugged';
+import { filters, waitForModule, getFunctionBySource } from './webpack';
+import { ModalComponents } from '../../types/components';
+import { ModuleExports, Filter, ObjectExports } from '@replugged';
 import { error } from "./logger";
-import type React from 'react';
 
-async function wrapFilter<T extends React.ComponentType>(name: string, filter: Filter): Promise<T> {
+async function wrapFilter<T extends ModuleExports | React.ComponentType>(name: string, filter: Filter, timeout: number = 5000): Promise<T> {
   return (await waitForModule(filter, {
-    timeout: 5_000,
+    timeout
   }).catch(() => {
     // eslint-disable-next-line no-undefined
     error("Replugged", "Components", undefined, `Could not find module ${name}`);
@@ -16,12 +16,27 @@ async function wrapFilter<T extends React.ComponentType>(name: string, filter: F
 const SwitchItem = wrapFilter<React.ComponentType>(
   "SwitchItem",
   filters.bySource(/=.\.helpdeskArticleId,.=.\.children/),
+  10000
 );
 
+const modal = wrapFilter("modal", filters.bySource("().closeWithCircleBackground")).then((mod) => {
+  if (!mod) return null;
+
+  return {
+    ModalRoot: getFunctionBySource("().root", mod as ObjectExports),
+    ModalHeader: getFunctionBySource("().header", mod as ObjectExports),
+    ModalContent: getFunctionBySource("().content", mod as ObjectExports),
+    ModalFooter: getFunctionBySource("().footerSeparator", mod as ObjectExports),
+    ModalCloseButton: getFunctionBySource("().closeWithCircleBackground", mod as ObjectExports),
+  }
+}) as Promise<ModalComponents>;
+
 export interface Components {
-  SwitchItem: React.ComponentType
+  SwitchItem: React.ComponentType,
+  modal: ModalComponents,
 }
 
 export default async (): Promise<Components> => ({
-  SwitchItem: await SwitchItem
+  SwitchItem: await SwitchItem,
+  modal: await modal,
 })
