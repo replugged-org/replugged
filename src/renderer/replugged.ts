75028d78918b1f32110810ef3a52c3d9c7ed8bb8
Albert Portnoy <albert@albertp.dev>
2022-12-18 15:46:52 -0600

    Fix webpack exports

--- src/renderer/replugged.ts
+++ src/renderer/replugged.ts
@@ -1,68 +1,10 @@
-import * as webpackModule from "./modules/webpack";
-import webpackCommon, { CommonModules } from "./modules/webpack-common";
-import components_, { Components } from "./modules/components";
-import { objectUtil } from "zod";
+import webpackCommon from "./modules/webpack-common";
+import components_ from "./modules/components";
 
 export * as injector from "./modules/injector";
 export { Injector } from "./modules/injector";
-export namespace webpack {
-  /**
-   * @see {@link CommonModules}
-   */
-  export let common: CommonModules = null as unknown as CommonModules;
-
-  export namespace filters {
-    export const {
-      /**
-       * Get a module that has all the given properties on one of its exports
-       * @param props List of property names
-       */
-      byProps,
-
-      /**
-       * Get a module whose source code matches the given string or RegExp
-       * @param match String or RegExp to match in the module's source code
-       *
-       * @remarks
-       * This function matches on the minified code, so make sure to keep that in mind when writing your strings/RegExp.
-       *
-       * Randomized variable names (usually 1-2 letters) are not stable between Discord updates. Make sure to use wildcards to make sure your RegExp matches if the variable name were to.
-       */ bySource,
-    } = webpackModule.filters;
-  }
-
-  export const {
-    /**
-     * @internal
-     * @hidden
-     */
-    wpRequire,
-    /**
-     * @internal
-     * @hidden
-     */
-    waitForReady,
-    /**
-     * @internal
-     * @hidden
-     */
-    waitForStart,
-    /**
-     * @internal
-     * @hidden
-     */
-    signalStart,
-
-    getById,
-    getByProps,
-    getBySource,
-    getModule,
-    getExportsForProps,
-    getFunctionBySource,
-    patchPlaintext,
-    waitForModule,
-  } = webpackModule;
-}
+const { webpack } = require("./modules/webpack");
+export { webpack };
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 webpackCommon().then((modules) => {
