51a345ef19559271c9d4779ae860c58c7147affa
FifiTheBulldog <fifithebulldog@outlook.com>
2022-12-21 00:28:31 -0500

    Let git history show the old NDW plaintext patch

--- src/renderer/coremods/noDevtoolsWarning/index.ts
+++ src/renderer/coremods/noDevtoolsWarning/index.ts
@@ -1,17 +1,3 @@
-/*
-Plaintext patch version of this coremod:
-
-{
-  find: "setDevtoolsCallbacks",
-  replacements: [
-    {
-      match: /if\(.{0,10}\|\|"0.0.0"!==.{0,2}\.remoteApp\.getVersion\(\)\)/,
-      replace: "if(false)"
-    }
-  ]
-}
-*/
-
 export function start(): void {
   DiscordNative.window.setDevtoolsCallbacks();
 }
