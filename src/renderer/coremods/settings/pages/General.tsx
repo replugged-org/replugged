0aef48e112c34dc8a1dcc46479d10129c8dcad9c
Albert Portnoy <albert@albertp.dev>
2022-12-21 11:35:14 -0600

    Better settings placeholder (#285)
    * Better settings placeholder
    
    * Update path
    
    Co-authored-by: FifiTheBulldog <63314105+FifiTheBulldog@users.noreply.github.com>
    
    Co-authored-by: FifiTheBulldog <63314105+FifiTheBulldog@users.noreply.github.com>

--- src/renderer/coremods/settings/pages/General.tsx
+++ src/renderer/coremods/settings/pages/General.tsx
@@ -1,3 +1,21 @@
+import React from "../../../modules/webpack/common/react";
+
 export const General = () => {
-  return <div>wake up wake up wake up</div>;
+  const [hue, setHue] = React.useState(0);
+  React.useEffect(() => {
+    const id = requestAnimationFrame(() => {
+      setHue((hue + 1) % 360);
+    });
+    return () => cancelAnimationFrame(id);
+  }, [hue]);
+
+  return (
+    <div style={{ color: "var(--text-normal)" }}>
+      Replugged settings are coming soon™.
+      <br />
+      <br />
+      <br />
+      <span style={{ color: `hsl(${hue}, 100%, 50%)` }}>Wake up. Wake up. Wake up.</span>
+    </div>
+  );
 };
