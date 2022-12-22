bb2b3a62650599e82636c6204f2b8989c6669a74
Albert Portnoy <albert@albertp.dev>
2022-12-21 22:36:53 -0600

    Badge coremod
    This should've probably been like 10 commits. Oops.

---
+++ src/renderer/coremods/badges/badges/Booster.tsx
@@ -0,0 +1,14 @@
+import React from "../../../modules/webpack/common/react";
+
+export default React.memo((props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38 38" {...props}>
+    <path
+      fill="currentColor"
+      d="m19.666,0.0005l-12.666,12.666l0,12.667l12.666,12.666l12.667,-12.666l0,-12.667l-12.667,-12.666zm9.5,24.035l-9.5,9.5l-9.499,-9.5l0,-10.07l9.499,-9.5l9.5,9.5l0,10.07z"
+    />
+    <path
+      fill="currentColor"
+      d="m13.333,15.2955l0,7.41l6.333,6.333l6.334,-6.333l0,-7.41l-6.334,-6.333l-6.333,6.333z"
+    />
+  </svg>
+));
---
+++ src/renderer/coremods/badges/badges/index.ts
@@ -0,0 +1,19 @@
+import Booster from "./Booster";
+import BugHunter from "./BugHunter";
+import Contributor from "./Contributor";
+import Developer from "./Developer";
+import EarlyUser from "./EarlyUser";
+import Staff from "./Staff";
+import Support from "./Support";
+import Translator from "./Translator";
+
+export default {
+  Booster,
+  BugHunter,
+  Contributor,
+  Developer,
+  EarlyUser,
+  Staff,
+  Support,
+  Translator,
+};
