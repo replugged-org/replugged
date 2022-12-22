bad6bd4922ec5cbc9daa09e71abe056c083b04e7
12944qwerty <qwerty12944qwerty@gmail.com>
2022-12-22 17:06:27 -0600

    Category Component and fixes

---
+++ src/renderer/modules/components/Category.tsx
@@ -0,0 +1,69 @@
+import { filters, waitForModule } from '../webpack';
+import { Divider, FormItem, FormText } from '.';
+import React from '../webpack/common/react';
+
+const classes = await waitForModule(filters.byProps("container", "labelRow", "title", "control", "note"), {timeout: 10000}) as Record<string, string>;
+
+type CategoryProps = {
+  title: string;
+  open?: boolean;
+  note?: string;
+  disabled?: boolean;
+  onChange?: () => void;
+};
+
+/**
+ * A category. It's, by default, is automatically handled by the component. `open` and `onChange` both must be specified to override.
+ * 
+ * @param props The props given to a Category
+ * @param props.title The title the category will show.
+ * @param props.children The components that the category reveals when opened.
+ * @param props.note The description the category will have.
+ * @param props.disabled If disabled, all events that occur to it will be disabled.
+ * @param props.open The default state of the category, or if props.onChange is specified, the state of the category determined by the developer.
+ * @param props.onChange The function that the developer can use to change the state of the Category. props.open must be specified for this to be called.
+ * @returns The Category Component.
+ */
+const Category = (props: React.PropsWithChildren<CategoryProps>) => {
+  const [ open, setOpen ] = React.useState(props.open || false);
+
+  const handleClick = () => {
+    if (props.disabled) return;
+
+    if (typeof props.onChange === 'function' && typeof props.open === 'boolean') props.onChange(); 
+    else setOpen(!open)
+  }
+
+  return (
+    <div className={classes.container}>
+      <FormItem style={{cursor: 'pointer', alignItems: 'center', display: 'flex'}}>
+        <div onClick={() => {handleClick()}}>
+          <div className={classes.labelRow}>
+            <label className={classes.title}>{props.title}</label>
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' style={{
+              width: 28,
+              height: 28,
+              marginRight: 15,
+              transition: 'transform 0.3s',
+              // eslint-disable-next-line no-undefined
+              transform: open ? 'rotate(90deg)' : undefined
+            }}>
+              <path
+                fill='var(--header-primary)'
+                d='M9.29 15.88L13.17 12 9.29 8.12c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0l4.59 4.59c.39.39.39 1.02 0 1.41L10.7 17.3c-.39.39-1.02.39-1.41 0-.38-.39-.39-1.03 0-1.42z' />
+            </svg>
+          </div>
+          <div className={classes.note}>
+            <FormText.DESCRIPTION>{props.note}</FormText.DESCRIPTION>
+          </div>
+        </div>
+      </FormItem>
+      {open ? 
+        <div style={{marginTop: 20, marginLeft: 12, borderLeft: '1px var(--background-modifier-accent) solid', paddingLeft: 33}}>{props.children}</div> :
+        <Divider className={classes.dividerDefault}/>
+      }
+    </div>
+  )
+}
+
+export default Category
