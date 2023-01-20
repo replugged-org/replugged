be7f73c2a850e2a95eff80541b769dfdb7eaf0bd
Albert Portnoy <albert@albertp.dev>
2023-01-19 19:40:55 -0600

    Add settings icon

--- src/renderer/coremods/settings/icons/index.ts
+++ src/renderer/coremods/settings/icons/index.ts
@@ -2,6 +2,7 @@ import Discord from "./Discord";
 import GitHub from "./GitHub";
 import Link from "./Link";
 import Reload from "./Reload";
+import Settings from "./Settings";
 import Trash from "./Trash";
 
 export default {
@@ -9,5 +10,6 @@ export default {
   GitHub,
   Link,
   Reload,
+  Settings,
   Trash,
 };
