16fcab48d6780576289c6126a968e5100b605018
Albert Portnoy <albert@albertp.dev>
2023-01-23 21:20:24 -0600

    Create quick CSS file if doesn't exist

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
-import { existsSync, mkdirSync } from "fs";
+import { existsSync, mkdirSync, writeFileSync } from "fs";
 import { join } from "path";
 
 const REPLUGGED_FOLDER_NAME = "replugged";
@@ -17,7 +17,6 @@ export const CONFIG_PATH = (() => {
 })();
 
 if (!existsSync(CONFIG_PATH)) {
-  console.log("Does not exist");
   mkdirSync(CONFIG_PATH);
 }
 
@@ -31,4 +30,9 @@ export const CONFIG_PATHS = Object.fromEntries(
     }
     return [name, path];
   }),
-) as Record<typeof CONFIG_FOLDER_NAMES[number], string>;
+) as Record<(typeof CONFIG_FOLDER_NAMES)[number], string>;
+
+const QUICK_CSS_FILE = join(CONFIG_PATHS.quickcss, "main.css");
+if (!existsSync(QUICK_CSS_FILE)) {
+  writeFileSync(QUICK_CSS_FILE, "");
+}
