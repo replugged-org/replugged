4c45295bb40c1eb7852b98467725fd70b7390bb7
Albert Portnoy <albert@albertp.dev>
2023-01-23 21:15:10 -0600

    catch missing css file

--- src/main/ipc/quick-css.ts
+++ src/main/ipc/quick-css.ts
@@ -6,7 +6,13 @@ import { CONFIG_PATHS } from "src/util";
 
 const CSS_PATH = join(CONFIG_PATHS.quickcss, "main.css");
 
-ipcMain.handle(RepluggedIpcChannels.GET_QUICK_CSS, () => readFile(CSS_PATH, { encoding: "utf-8" }));
+ipcMain.handle(RepluggedIpcChannels.GET_QUICK_CSS, () => {
+  try {
+    return readFile(CSS_PATH, { encoding: "utf-8" });
+  } catch {
+    return "";
+  }
+});
 ipcMain.on(RepluggedIpcChannels.SAVE_QUICK_CSS, (_, css: string) =>
   writeFile(CSS_PATH, css, { encoding: "utf-8" }),
 );
