861b840623cbbf10638f28678767d55b45d99d2e
Harley <GriefMoDz@gmail.com>
2023-01-28 10:46:50 +1100

    refactor(ipc): Use ES6 `import` instead of `require`

--- src/main/ipc/i18n.ts
+++ src/main/ipc/i18n.ts
@@ -1,8 +1,10 @@
 import { ipcMain } from "electron";
 import { RepluggedIpcChannels } from "../../types";
 
-ipcMain.handle(RepluggedIpcChannels.GET_I18N_STRINGS, () => require("../../../i18n").default);
-ipcMain.handle(
-  RepluggedIpcChannels.GET_I18N_OVERRIDES,
-  () => require("../../../i18n/overrides").default,
-);
+// @ts-expect-error No declaration file
+import strings from "../../../i18n";
+// @ts-expect-error No declaration file
+import overrides from "../../../i18n/overrides";
+
+ipcMain.handle(RepluggedIpcChannels.GET_I18N_STRINGS, () => strings);
+ipcMain.handle(RepluggedIpcChannels.GET_I18N_OVERRIDES, () => overrides);
