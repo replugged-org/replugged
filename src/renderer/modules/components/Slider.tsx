42ff71b350430157d78a50f5bce59b144e07d1cb
cafeed28 <artlekseev@gmail.com>
2023-01-31 12:06:08 +0500

    Slider component wrapper Add className property

--- src/renderer/modules/components/Slider.tsx
+++ src/renderer/modules/components/Slider.tsx
@@ -1,6 +1,7 @@
+import { ReactComponent } from "src/types";
 import { filters, waitForModule } from "../webpack";
 
-interface SliderProps {
+interface SliderCompProps {
   disabled?: boolean;
   markers?: number[];
   stickToMarkers?: boolean;
@@ -10,16 +11,30 @@ interface SliderProps {
   maxValue?: number;
   mini?: boolean;
   style?: React.CSSProperties;
+  className?: string;
   onValueChange?: (e: number) => void;
   asValueChanges?: (e: number) => void;
   onValueRender?: (e: number) => string;
   onMarkerRender?: (e: number) => string;
 }
 
-export type SliderType = React.ComponentType<SliderProps> & {
-  defaultProps: SliderProps;
+type SliderCompType = React.ComponentType<SliderCompProps> & {
+  defaultProps: SliderCompProps;
 };
 
-export default (await waitForModule(filters.bySource(".moveGrabber=")).then((mod) =>
+const SliderComp = (await waitForModule(filters.bySource(".moveGrabber=")).then((mod) =>
   Object.values(mod).find((x) => "defaultProps" in x && "stickToMarkers" in x.defaultProps),
-)) as SliderType;
+)) as SliderCompType;
+
+interface SliderProps extends SliderCompProps {
+  value?: number;
+  onChange?: (e: number) => void;
+}
+
+type SliderType = ReactComponent<SliderProps>;
+
+export default ((props) => {
+  return (
+    <SliderComp {...props} initialValue={props.value} onValueChange={props.onChange}></SliderComp>
+  );
+}) as SliderType;
