4485c9d101eb806cae2d07a8781f2b21e9a5ca89
FedeIlLeone <dileopolus@virgilio.it>
2023-02-05 12:40:10 +0100

    fix(category-component): style and children type

--- src/renderer/modules/components/Category.tsx
+++ src/renderer/modules/components/Category.tsx
@@ -1,5 +1,5 @@
 import { filters, waitForModule } from "../webpack";
-import { Divider, Flex, FormItem, FormText } from ".";
+import { Divider, Flex, FormText } from ".";
 import React from "@common/react";
 import type { ObjectExports } from "../../../types";
 
@@ -15,7 +15,7 @@ interface CategoryProps {
   onChange?: () => void;
 }
 
-export type CategoryType = React.FC<CategoryProps>;
+export type CategoryType = React.FC<React.PropsWithChildren<CategoryProps>>;
 
 /**
  * A category. It's opened state, by default, is automatically handled by the component. `open` and `onChange` both must be specified to override.
@@ -31,50 +31,47 @@ export default ((props: React.PropsWithChildren<CategoryProps>) => {
   };
 
   return (
-    <div
-      className={`${Flex.Direction.VERTICAL} ${Flex.Justify.START} ${Flex.Align.STRETCH} ${Flex.Wrap.NO_WRAP}`}>
-      <FormItem>
-        <div
-          style={{ cursor: "pointer", alignItems: "center", display: "flex" }}
-          onClick={() => {
-            handleClick();
+    <div style={{ marginBottom: 20 }}>
+      <div
+        style={{ cursor: "pointer", alignItems: "center", display: "flex" }}
+        onClick={() => {
+          handleClick();
+        }}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          style={{
+            width: 28,
+            height: 28,
+            marginRight: 15,
+            transition: "transform 0.3s",
+            transform: open ? "rotate(90deg)" : undefined,
           }}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            style={{
-              width: 28,
-              height: 28,
-              marginRight: 15,
-              transition: "transform 0.3s",
-              transform: open ? "rotate(90deg)" : undefined,
-            }}>
-            <path
-              fill="var(--header-primary)"
-              d="M9.29 15.88L13.17 12 9.29 8.12c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0l4.59 4.59c.39.39.39 1.02 0 1.41L10.7 17.3c-.39.39-1.02.39-1.41 0-.38-.39-.39-1.03 0-1.42z"
-            />
-          </svg>
-          <div>
-            <div className={classes.labelRow}>
-              <label className={classes.title}>{props.title}</label>
-            </div>
-            <FormText.DESCRIPTION className={classes.note}>{props.note}</FormText.DESCRIPTION>
+          <path
+            fill="var(--header-primary)"
+            d="M9.29 15.88L13.17 12 9.29 8.12c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0l4.59 4.59c.39.39.39 1.02 0 1.41L10.7 17.3c-.39.39-1.02.39-1.41 0-.38-.39-.39-1.03 0-1.42z"
+          />
+        </svg>
+        <div style={{ flex: 1 }}>
+          <div className={classes.labelRow}>
+            <label className={classes.title}>{props.title}</label>
           </div>
+          <FormText.DESCRIPTION className={classes.note}>{props.note}</FormText.DESCRIPTION>
         </div>
-        {open ? (
-          <div
-            style={{
-              marginTop: 20,
-              marginLeft: 12,
-              borderLeft: "1px var(--background-modifier-accent) solid",
-              paddingLeft: 33,
-            }}>
-            {props.children}
-          </div>
-        ) : (
-          <Divider className={classes.dividerDefault} />
-        )}
-      </FormItem>
+      </div>
+      {open ? (
+        <div
+          style={{
+            marginTop: 20,
+            marginLeft: 12,
+            borderLeft: "1px var(--background-modifier-accent) solid",
+            paddingLeft: 33,
+          }}>
+          {props.children}
+        </div>
+      ) : (
+        <Divider className={classes.dividerDefault} />
+      )}
     </div>
   );
 }) as CategoryType;
