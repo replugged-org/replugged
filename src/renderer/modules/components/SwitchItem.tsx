0fa90f35a1b91ab93f8235daf4d91a84119b7273
FedeIlLeone <dileopolus@virgilio.it>
2023-02-11 23:48:48 +0100

    feat(switchitem): types

--- src/renderer/modules/components/SwitchItem.tsx
+++ src/renderer/modules/components/SwitchItem.tsx
@@ -1,23 +1,31 @@
 import type { ObjectExports, ReactComponent } from "../../../types";
 import { filters, getFunctionBySource, waitForModule } from "../webpack";
 
-export type SwitchItemType = ReactComponent<{
-  note?: string;
-  value: boolean;
-  onChange: (e: boolean) => void;
-  disabled?: boolean;
-  style?: React.CSSProperties;
-  className?: string;
-  tooltipNode?: string;
-}>;
-
 export type SwitchType = ReactComponent<{
   checked: boolean;
   onChange: (e: boolean) => void;
   disabled?: boolean;
-  style?: React.CSSProperties;
   className?: string;
 }>;
+
+interface SwitchItemCompProps {
+  value: boolean;
+  onChange: (e: boolean) => void;
+  note?: string;
+  tooltipNote?: string;
+  disabled?: boolean;
+  hideBorder?: boolean;
+  style?: React.CSSProperties;
+  className?: string;
+}
+
+type SelectCompType = React.ComponentType<SwitchItemCompProps>;
+
+interface SwitchItemProps extends SwitchItemCompProps {
+  divider?: boolean;
+}
+
+export type SwitchItemType = React.FC<React.PropsWithChildren<SwitchItemProps>>;
 
 const switchModStr = "xMinYMid meet";
 const switchRgx = /{className:\w+\(\)\(\w+,\w+\.className\)}/;
@@ -28,7 +36,12 @@ export const Switch = (await waitForModule(filters.bySource(switchModStr)).then(
   return getFunctionBySource(mod as ObjectExports, switchRgx);
 })) as SwitchType;
 
-export const SwitchItem = (await waitForModule(filters.bySource(switchItemStr)).then((mod) => {
+const SwitchItemComp = (await waitForModule(filters.bySource(switchItemStr)).then((mod) => {
   if (typeof mod === "function") return mod;
   return getFunctionBySource(mod as ObjectExports, switchItemStr);
-})) as SwitchItemType;
+})) as SelectCompType;
+
+export const SwitchItem = ((props) => {
+  if (props.divider) props.hideBorder = props.divider;
+  return <SwitchItemComp {...props} />;
+}) as SwitchItemType;
