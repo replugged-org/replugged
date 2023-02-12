46671704c5d8d4fb613e3cacfb8cc389f5e7c44e
Albert Portnoy <albert@albertp.dev>
2023-02-11 18:20:52 -0600

    :facepalm: forgot to promise this one

--- src/renderer/modules/webpack/index.ts
+++ src/renderer/modules/webpack/index.ts
@@ -412,7 +412,7 @@ export async function waitForModule<
   T extends RawModule | ModuleExports = RawModule | ModuleExports,
 >(filter: Filter, options: WaitForOptions = {}): Promise<T> {
   const existing = getModule(filter, { all: false, raw: options.raw }) as
-    | (typeof options["raw"] extends true ? T & RawModule : T & ModuleExports)
+    | ((typeof options)["raw"] extends true ? T & RawModule : T & ModuleExports)
     | undefined;
   if (existing) {
     return Promise.resolve(existing);
@@ -579,7 +579,7 @@ export function waitForProps<
 export function waitForProps<
   P extends string = string,
   T extends ModuleExportsWithProps<P> = ModuleExportsWithProps<P>,
->(...props: P[]): T;
+>(...props: P[]): Promise<T>;
 
 /**
  * Like {@link getByProps} but waits for the module to be loaded.
