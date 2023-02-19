802afbf8e55d9d06a51c46524ad1a325a7737841
colin273 <colin273273@outlook.com>
2023-02-19 00:28:06 -0500

    Expose msg popover directly on replugged global

--- src/renderer/replugged.ts
+++ src/renderer/replugged.ts
@@ -25,7 +25,41 @@ export { default as notices } from "./apis/notices";
 export { default as commands } from "./apis/commands";
 export * as settings from "./apis/settings";
 
-export * as api from "./apis";
+/**
+ * A quick method to add a button to any message popover.
+ * @param item The function that creates the button to add
+ * @returns Uninject Function.
+ *
+ * @example
+ * ```
+ * import { api, types } from "replugged";
+ *
+ * let removeButton;
+ * let item: types.GetButtonItem = (msg: Message, channel: Channel) => {
+ *   return {
+ *     label: "Click the button!",
+ *     icon: <svg></svg>, // Cool icon
+ *     onClick: () => {
+ *       // do stuff here when someone left clicks the button
+ *     },
+ *     onContextMenu: () => {
+ *       // do other stuff here when someone right clicks the button
+ *     },
+ *   };
+ * };
+ *
+ * function start() {
+ *   removeButton = api.messagePopover.addButton(item, "optional unique key here");
+ * }
+ *
+ * function stop() {
+ *   removeButton();
+ *   // or you can use
+ *   // api.messagePopover.removeButton(item)
+ * }
+ * ```
+ */
+export * as messagePopover from "./apis/message-popover";
 
 /**
  * @internal
