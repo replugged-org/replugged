67773b8c6a95852781b998e23cdd3ffe389d0d10
12944qwerty <qwerty12944qwerty@gmail.com>
2023-03-06 21:48:22 -0600

    popover api (#338)
    * popover api
    
    * changes
    
    * function replacement
    
    * stuff
    
    * lint
    
    * namespacify the api
    
    * improve docs
    
    * bruh lint was complaining about comments fu
    
    * randomize button keys
    
    * fu lint
    
    * move key generation
    
    * dom iterable... happy gh checks?
    
    * insert cool no consolelog message here
    
    * Don't need to export a namespace to get docs
    
    * Empty commit to trigger netlify
    
    * Fix error
    
    * hopefully more permenant regex
    
    * linting
    
    * Expose msg popover directly on replugged global
    
    * move to coremods
    
    * fix linter
    
    * cspell fix
    
    ---------
    
    Co-authored-by: colin273 <colin273273@outlook.com>
    Co-authored-by: Albert Portnoy <albert@albertp.dev>
    Co-authored-by: Colin <63314105+colin273@users.noreply.github.com>

---
+++ src/types/coremods/message.ts
@@ -0,0 +1,14 @@
+import { Channel, Message } from "discord-types/general";
+import { MouseEventHandler } from "react";
+
+export interface ButtonItem {
+  key?: string;
+  label: string;
+  icon: React.ComponentType<unknown>;
+  message?: Message;
+  channel?: Channel;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  onContextMenu?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export type GetButtonItem = (message: Message, channel: Channel) => ButtonItem | null;
