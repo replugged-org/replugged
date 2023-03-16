e9e41ebc4712c1a02a79f216276a0457539e103d
Albert Portnoy <albert@albertp.dev>
2023-03-16 17:29:25 -0500

    fix(common): fix constants.CSSVariables

--- src/renderer/modules/common/constants.ts
+++ src/renderer/modules/common/constants.ts
@@ -21,11 +21,38 @@ export const RPCEvents = getExportsForProps<string, Record<string, string>>(raw,
 ])!;
 export const Colors = getExportsForProps<string, Record<string, string>>(raw, ["GREY1", "GREY2"])!;
 export const Status = getExportsForProps<string, Record<string, string>>(raw, ["ONLINE", "IDLE"])!;
-export const CSSVariables = getExportsForProps<string, Record<string, string>>(raw, [
-  "TEXT_NORMAL",
-  "BACKGROUND_PRIMARY",
-])!;
 export const Paths = getExportsForProps<string, Record<string, string>>(raw, [
   "INDEX",
   "DOWNLOADS",
 ])!;
+
+interface ColorResponse {
+  hex: () => string;
+  hsl: () => string;
+  int: () => number;
+  spring: () => string;
+}
+
+interface Color {
+  css: string;
+  resolve: (theme: { theme: string; saturation: number }) => ColorResponse;
+}
+
+interface UnsafeRawColor {
+  css: string;
+  resolve: (theme: { saturation: number }) => ColorResponse;
+}
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+type ColorMod = {
+  themes: Record<string, string>;
+  colors: Record<string, Color>;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  unsafe_rawColors: Record<string, UnsafeRawColor>;
+};
+
+export const CSSVariables = await waitForModule<ColorMod>(
+  filters.bySource(/\w+\.unsafe_rawColors\[\w+\]\.resolve\(\w+\)/),
+);
+
+export const Themes = CSSVariables.themes;
