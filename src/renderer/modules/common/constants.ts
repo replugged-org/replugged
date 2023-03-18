f6e5299abedd3691dc704e024e3e8a5627f66b2a
Albert Portnoy <albert@albertp.dev>
2023-03-18 09:04:31 -0500

    fix(common): fix constants.CSSVariables (#429)

--- src/renderer/modules/common/constants.ts
+++ src/renderer/modules/common/constants.ts
@@ -1,4 +1,4 @@
-import { filters, getExportsForProps, waitForModule } from "../webpack";
+import { filters, getExportsForProps, waitForModule, waitForProps } from "../webpack";
 
 export const raw = await waitForModule(filters.bySource("BASE_URL:"));
 
@@ -21,11 +21,43 @@ export const RPCEvents = getExportsForProps<string, Record<string, string>>(raw,
 ])!;
 export const Colors = getExportsForProps<string, Record<string, string>>(raw, ["GREY1", "GREY2"])!;
 export const Status = getExportsForProps<string, Record<string, string>>(raw, ["ONLINE", "IDLE"])!;
-export const CSSVariables = getExportsForProps<string, Record<string, string>>(raw, [
-  "TEXT_NORMAL",
-  "BACKGROUND_PRIMARY",
-])!;
 export const Paths = getExportsForProps<string, Record<string, string>>(raw, [
   "INDEX",
   "DOWNLOADS",
 ])!;
+
+export const CSSVariables = await waitForProps<string, Record<string, string>>(
+  "TEXT_NORMAL",
+  "BACKGROUND_PRIMARY",
+);
+
+interface ColorResponse {
+  hex: () => string;
+  hsl: () => string;
+  int: () => number;
+  spring: () => string;
+}
+
+interface Color {
+  css: string;
+  resolve: (theme: { theme: string; saturation: number }) => ColorResponse;
+}
+
+interface UnsafeRawColor {
+  css: string;
+  resolve: (theme: { saturation: number }) => ColorResponse;
+}
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+type ColorMod = {
+  themes: Record<string, string>;
+  colors: Record<string, Color>;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  unsafe_rawColors: Record<string, UnsafeRawColor>;
+};
+
+export const ColorGenerator = await waitForModule<ColorMod>(
+  filters.bySource(/\w+\.unsafe_rawColors\[\w+\]\.resolve\(\w+\)/),
+);
+
+export const Themes = ColorGenerator.themes;
