eeaf29accfabdf3ef90a82eb51cfc3f2c81c720f
Albert Portnoy <albert@albertp.dev>
2023-04-13 15:57:36 -0500

    optional chaining to prevent errors

--- src/renderer/coremods/badges/index.tsx
+++ src/renderer/coremods/badges/index.tsx
@@ -95,7 +95,7 @@ export async function start(): Promise<void> {
           return res;
         }
 
-        const firstChild = res.props.children[0];
+        const firstChild = res.props?.children?.[0];
         if (!firstChild || !Array.isArray(firstChild)) {
           logger.error("Error injecting badges: res.props.children[0] is not an array", { res });
           return res;
