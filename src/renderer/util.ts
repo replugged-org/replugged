e45a6a02cb171b1a61822225bc1078cb9513aa71
12944qwerty <qwerty12944qwerty@gmail.com>
2023-05-07 22:30:25 -0500

    findInReactTree implementation

--- src/renderer/util.ts
+++ src/renderer/util.ts
@@ -262,3 +262,54 @@ export function virtualMerge<O extends ObjectType[]>(...objects: O): ExtractObje
   }
   return new Proxy(fallback, handler) as ExtractObjectType<O>;
 }
+
+type Tree = React.ComponentType | React.ComponentType[] | Record<string, Tree>;
+type TreeFilter = string | ((tree: Tree) => boolean);
+
+/**
+ * All credit goes to rauenzi (Zerebos#7790) for writing up this implementation.
+ * You can find the original source here:
+ * <https://github.com/rauenzi/BDPluginLibrary/blob/683d6abc70f149a39e2f0433ffb65e55ca47c4e3/release/0PluginLibrary.plugin.js#L2585C15-L2611>
+ * 
+ * @remarks Used mainly in findInReactTree
+*/
+export function findInTree (tree: Tree, searchFilter: TreeFilter, args: { walkable?: string[], ignore?: string[] } = {}): React.ComponentType | null | undefined {
+  const { walkable, ignore } = args;
+
+  if (typeof searchFilter === "string") {
+      if (Object.prototype.hasOwnProperty.call(tree, searchFilter)) return tree[searchFilter];
+  }
+  else if (searchFilter(tree)) {
+      return tree;
+  }
+
+  if (typeof tree !== "object" || tree == null) return undefined;
+
+  let tempReturn;
+  if (Array.isArray(tree)) {
+      for (const value of tree) {
+          tempReturn = findInTree(value, searchFilter, {walkable, ignore});
+          if (typeof tempReturn != "undefined") return tempReturn;
+      }
+  }
+  else {
+      const toWalk = walkable == null ? Object.keys(tree) : walkable;
+      for (const key of toWalk) {
+          if (!Object.prototype.hasOwnProperty.call(tree, key) || ignore?.includes(key)) continue;
+          tempReturn = findInTree(tree[key], searchFilter, {walkable, ignore});
+          if (typeof tempReturn != "undefined") return tempReturn;
+      }
+  }
+  return tempReturn;
+}
+
+/**
+ * Find the component you are looking for in a tree, recursively.
+ * 
+ * @param tree The tree to search through
+ * @param searchFilter The filter. Either a string or a function
+ * @returns The component you are looking for
+ */
+export function findInReactTree(tree: Tree, searchFilter: TreeFilter): React.ComponentType | null | undefined {
+  return findInTree(tree, searchFilter, { walkable: ["props", "children", "child", "sibling"] });
+}
