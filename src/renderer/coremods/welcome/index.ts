33ac2492e7926f055bea9fdfc79a9057ce882328
Dominic Ruggiero <dominic@userexe.me>
2023-07-02 13:39:54 -0400

    add requested changes

--- src/renderer/coremods/welcome/index.ts
+++ src/renderer/coremods/welcome/index.ts
@@ -1,9 +1,20 @@
 import { generalSettings } from "../settings/pages/General";
-import { notices } from "@replugged";
+import { notices, util } from "@replugged";
 import { Messages } from "@common/i18n";
 
 export function start(): void {
   if (!generalSettings.get("showWelcomeNoticeOnOpen")) return;
-  notices.sendAnnouncement({ message: Messages.REPLUGGED_WELCOME_NOTICE });
-  generalSettings.set("showWelcomeNoticeOnOpen", false);
+  notices.sendAnnouncement({
+    message: Messages.REPLUGGED_NOTICES_WELCOME_NEW_USER,
+    button: {
+      text: Messages.REPLUGGED_NOTICES_JOIN_SERVER_BUTTON,
+      onClick: () => {
+        util.goToOrJoinServer("replugged");
+        generalSettings.set("showWelcomeNoticeOnOpen", false);
+      },
+    },
+    onClose: () => {
+      generalSettings.set("showWelcomeNoticeOnOpen", false);
+    },
+  });
 }
