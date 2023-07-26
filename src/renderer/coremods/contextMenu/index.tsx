482e3dccf1a28f2a418c4b97b5edf46df0975cb5
Tharki-God <alokender018@gmail.com>
2023-07-26 06:05:23 +0530

    handle adding props at right place. would add for submenu items too now

--- src/renderer/coremods/contextMenu/index.tsx
+++ src/renderer/coremods/contextMenu/index.tsx
@@ -16,26 +16,35 @@ export const menuItems = {} as Record<
   Array<{ getItem: GetContextItem; sectionId: number; indexInSection: number }>
 >;
 
+type RepluggedContextItem = ContextItem & {
+  props: { replug?: boolean; id?: string; };
+}
+
 /**
  * Converts data into a React element. Any elements or falsy value will be returned as is
  * @param raw The data to convert
  * @returns The converted item
  */
-function makeItem(raw: RawContextItem | ContextItem | undefined | void): ContextItem | undefined {
+function makeItem(raw: RawContextItem | ContextItem | undefined | void): RepluggedContextItem | undefined {
   // Occasionally React won't be loaded when this function is ran, so we don't return anything
   if (!React) return undefined;
 
   if (!raw) {
     // If something falsy is passed, let it through
     // Discord just skips over them too
-    return raw as ContextItem | undefined;
+    return raw as undefined;
   }
   if (React.isValidElement(raw)) {
     // We can't construct something that's already made
-    return raw as ContextItem | undefined;
+    // apparently you can but we ain't gonna do that because states will get messed up.
+    return raw as RepluggedContextItem;
   }
 
   const { type, ...props } = raw;
+    // add in prop  for cleanup
+    props.replug = true;
+    // custom unique id if not added by dev
+    props.id ??= `repluggedMenuItem-${Number(`0.${Date.now()}`).toString(36).substring(2)}`;  
   if (props.children) {
     props.children = props.children.map((child: RawContextItem | ContextItem | undefined) =>
       makeItem(child),
@@ -139,17 +148,7 @@ export function _buildPatchedMenu(menu: ContextMenuData): React.ReactElement | n
   //adding new items
   menuItems[navId].forEach((item) => {
     try {
-      const itemRet = makeItem(item.getItem(data, menu)) as ContextItem & {
-        props: { replug?: boolean; id?: string; };
-      };
-
-      if (itemRet?.props) {
-        // add in prop  for cleanup
-        itemRet.props.replug = true;
-        // custom unique id if not added by dev
-        itemRet.props.id ??= `repluggedMenuItem-${Number(`0.${Date.now()}`).toString(36).substring(2)}`
-      }
-
+      const itemRet = makeItem(item.getItem(data, menu));     
       menu.children.at(item.sectionId)?.props.children?.splice(item.indexInSection, 0, itemRet);
     } catch (err) {
       logger.error("Error while running GetContextItem function", err, item.getItem, menu.children.at(item.sectionId));
