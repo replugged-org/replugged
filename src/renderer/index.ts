a74149b4b625e3521c0d777d422d486db9355e47
EastArctica <eastarctica@gmail.com>
2023-08-18 23:18:38 -0400

    Implement better splash detection method

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -2,8 +2,12 @@ import * as replugged from "./replugged";
 
 window.replugged = replugged;
 
+type DiscordSplashWindow = Window & {
+  DiscordSplash?: object;
+};
+
 // Splash screen
-if (document.title === "Discord Updater") {
+if ((window as DiscordSplashWindow).DiscordSplash) {
   await replugged.ignition.startSplash();
 } else {
   await replugged.plugins.loadAll();
