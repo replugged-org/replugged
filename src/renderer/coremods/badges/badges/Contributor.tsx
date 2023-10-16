1a6b242454f3fe4866c652aa2d5cc63111954cc3
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2023-10-16 01:54:30 +0100

    Fix styling

--- src/renderer/coremods/badges/badges/Contributor.tsx
+++ src/renderer/coremods/badges/badges/Contributor.tsx
@@ -1,4 +1,4 @@
-import React, {_} from "@common/react";
+import React, { _ } from "@common/react";
 
 export default React.memo((props) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
