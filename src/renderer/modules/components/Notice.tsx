2278e7bf92af27139981cd0c5530ebb36b2d197a
Tharki-God <alokender018@gmail.com>
2023-10-28 23:42:28 +0530

    no one saw anything

--- src/renderer/modules/components/Notice.tsx
+++ src/renderer/modules/components/Notice.tsx
@@ -2,6 +2,13 @@ import type React from "react";
 import { filters, waitForModule } from "../webpack";
 import type { Variant } from "./Text";
 
+const Types = {
+  WARNING: 0,
+  INFO: 1,
+  ERROR: 2,
+  POSITIVE: 3,
+} as const;
+
 interface NoticeProps {
   children: React.ReactNode;
   messageType: (typeof Types)[keyof typeof Types];
@@ -14,7 +21,7 @@ export type NoticeType = React.FC<NoticeProps> & {
   Types: typeof Types; // for backwards compat
   HelpMessageTypes: typeof Types;
   default: React.FC<NoticeProps>;
-}
+};
 
 const NoticeComp = await waitForModule<NoticeType>(filters.bySource("WARNING=0]"));
 const Notice = NoticeComp.default as NoticeType;
