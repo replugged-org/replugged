a62f1b43b5d6ef18c8f93b5a9b95d53fbdf5c195
East_Arctica <eastarctica@gmail.com>
2024-03-01 15:24:52 -0500

    Fix advanced settings crash

--- src/renderer/coremods/settings/pages/General.tsx
+++ src/renderer/coremods/settings/pages/General.tsx
@@ -190,12 +190,6 @@ export const General = (): React.ReactElement => {
           {Messages.REPLUGGED_SETTINGS_REACT_DEVTOOLS}
         </SwitchItem>
 
-        <SwitchItem
-          {...util.useSetting(generalSettings, "transparentWindow")}
-          note={Messages.REPLUGGED_SETTINGS_TRANSPARENT_DESC}>
-          {Messages.REPLUGGED_SETTINGS_TRANSPARENT}
-        </SwitchItem>
-
         <ButtonItem
           button={Messages.REPLUGGED_SETTINGS_DEV_COMPANION_RECONNECT}
           note={Messages.REPLUGGED_SETTINGS_DEV_COMPANION_DESC}
