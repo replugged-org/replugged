e8eabaeedd2d747d99d7a91a9c5be6a7dd05ec05
East_Arctica <eastarctica@gmail.com>
2024-03-01 15:25:39 -0500

    Use built in electron blur if available.

--- src/main/ipc/transparency.ts
+++ src/main/ipc/transparency.ts
@@ -1,11 +1,27 @@
 import { BrowserWindow, ipcMain } from "electron";
 import { RepluggedIpcChannels } from "../../types";
-// @ts-expect-error Type defs are obtained through @pyke/vibe
-import vibePath from "../../vibe.node";
+import os from "os";
 
 let vibe: typeof import("@pyke/vibe");
-if (process.platform === "win32") {
-  vibe = require(vibePath) as unknown as typeof import("@pyke/vibe");
+let release = os.release().split(".").map(Number);
+let usesVibe =
+  // Windows
+  process.platform === "win32" &&
+  // Before Electron 24
+  Number(process.versions.electron.split(".")[0]) < 24 &&
+  // Before Windows 11 22H2+
+  release[0] <= 10 && // 11 doesn't exist yet but it could.
+  release[1] < 22621;
+
+if (usesVibe) {
+  // @ts-expect-error Type defs are obtained through @pyke/vibe
+  import("../../vibe.node")
+    .then((module) => {
+      vibe = module as unknown as typeof import("@pyke/vibe");
+    })
+    .catch((error) => {
+      console.error("Failed to load vibe.", error);
+    });
 }
 
 let currentEffect: Parameters<typeof vibe.applyEffect>[1] | null = null;
@@ -29,7 +45,18 @@ ipcMain.handle(
     }
 
     let windows = BrowserWindow.getAllWindows();
-    windows.forEach((window) => vibe.applyEffect(window, effect));
+    windows.forEach((window) => {
+      if (usesVibe) {
+        // The valid options for setBackgroundMaterial are "auto" | "none" | "mica" | "acrylic" | "tabbed".
+        // This goes against vibe which allows for "unified-acrylic" and "blurbehind".
+        // Also, vibe does not allow for "auto", "none" or "tabbed"
+
+        // @ts-expect-error Only exists in electron 24+, our types don't have this.
+        window.setBackgroundMaterial(effect === NULL ? "none" : effect); // NULL is used to disable.
+      } else {
+        vibe.applyEffect(window, effect);
+      }
+    });
     currentEffect = effect;
   },
 );
