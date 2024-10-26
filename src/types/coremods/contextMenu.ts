88b5bd0aa92d13a43a81b1bbb14c750a63a6b329
Federico <38290480+FedeIlLeone@users.noreply.github.com>
2024-10-26 10:17:55 +0200

    fix: types for raw context menu items

--- src/types/coremods/contextMenu.ts
+++ src/types/coremods/contextMenu.ts
@@ -3,19 +3,20 @@ import type React from "react";
 
 type ContextMenuComponents = Omit<ContextMenuType, "ItemColors" | "ContextMenu">;
 
-interface BaseRawContextItem<T> {
-  type: T;
-}
+type RawContextMenuProps = {
+  [K in keyof ContextMenuComponents]: React.ComponentProps<ContextMenuComponents[K]> & {
+    type: ContextMenuComponents[K];
+  };
+};
 
 type WithRawChildren<T> = T extends { children: React.ReactNode }
   ? Omit<T, "children"> & { children: RawContextItem | RawContextItem[] }
   : T;
 
-// TODO: Type is not correctly extending the props of the component based on the generic T
 export type RawContextItem<
   T extends
-    ContextMenuComponents[keyof ContextMenuComponents] = ContextMenuComponents[keyof ContextMenuComponents],
-> = BaseRawContextItem<T> & WithRawChildren<React.ComponentProps<T>>;
+    RawContextMenuProps[keyof RawContextMenuProps] = RawContextMenuProps[keyof RawContextMenuProps],
+> = WithRawChildren<T>;
 
 export type GetContextItem<T extends Record<string, unknown> = Record<string, unknown>> = (
   data: T,
