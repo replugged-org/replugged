3f3c22665c8394b6e083b61daf07754e7fe10876
yofukashino <alokender018@gmail.com>
2025-03-08 01:29:03 +0530

    fix: styling

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,6 @@ export const DISCORD_INVITE = "SgKSKyh9gY"; // yofukashino's server for time bei
 export const WEBLATE_URL = "https://i18n.replugged.dev";
 export const WEBSITE_URL = "https://replugged.dev"; // leave this be?
 export const BETA_WEBSITE_URL = "https://beta.replugged.dev";
-export const DISCORD_BLURPLE = "#4752c4";
+export const DISCORD_BLURPLE = "#5865f2";
 export const RECELLED_CLYDE_ID = "8269676976766968"; // "RECELLED" in ASCII codes
 export const DEFAULT_LOCALE = "en-US";
