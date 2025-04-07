389564a0d874dcfa217c4355c64c3362d7d297cb
Federico <38290480+fedeericodl@users.noreply.github.com>
2025-04-07 19:51:49 +0200

    fix: handle "login handoff" state

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -2,9 +2,8 @@ import * as replugged from "./replugged";
 
 window.replugged = replugged;
 
-const allowedHost = "discord.com";
-const { hostname } = new URL(window.location.href);
-if (hostname === allowedHost || hostname.endsWith(`.${allowedHost}`)) {
+const allowedHosts = ["discord.com", "discordapp.com"];
+if (allowedHosts.some((host) => window.location.hostname.endsWith(host))) {
   replugged.ignition.ignite();
 } else {
   replugged.ignition.startSplash();
