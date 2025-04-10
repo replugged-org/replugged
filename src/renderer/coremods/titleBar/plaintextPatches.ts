b35bf90ee105fae89cc45ba44c7610dafb14936e
yofukashino <alokender018@gmail.com>
2025-04-10 21:02:32 +0530

    fix: plaintext

--- src/renderer/coremods/titleBar/plaintextPatches.ts
+++ src/renderer/coremods/titleBar/plaintextPatches.ts
@@ -8,22 +8,26 @@ const generalSettings = init<GeneralSettings, keyof typeof defaultSettings>(
 
 const replacements = [
   {
-    match: /\(0,.\.getPlatform\)\(\)/,
+    match: /\(0,.{1,3}\.getPlatform\)\(\)/g,
     replace: `"WINDOWS"`,
   },
 ];
 
 export default (navigator.userAgent.includes("Linux") && generalSettings.get("titleBar")
   ? [
+      // Pre Discord Refresh
       {
         find: ".appAsidePanelWrapper,",
         replacements,
       },
+      // Discord Refresh
       {
         find: ".winButtons",
         replacements,
       },
+      // Popouts
       { find: "this.registerPopoutGlobalKeybinds", replacements },
+      // Disabling Native Titlebar in popouts
       {
         find: "menubar:!1,toolbar:!1",
         replacements: [
