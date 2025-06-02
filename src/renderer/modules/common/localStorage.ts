8edbeb41c323134c304a96d9061ede3c24454074
Federico <38290480+fedeericodl@users.noreply.github.com>
2025-06-02 20:38:08 +0200

    refactor: use types for LocalStorage instead of ObjectStorage

--- src/renderer/modules/common/localStorage.ts
+++ src/renderer/modules/common/localStorage.ts
@@ -1,17 +1,16 @@
 import { filters, getExportsForProps, waitForModule } from "../webpack";
 
 export declare class LocalStorage {
-  public get<T>(key: string, fallback?: T): T;
-  public set(key: string, value: unknown): void;
-  public remove(key: string): void;
-  public clear(): void;
-  public stringify(): string;
-  public asyncGetRaw(key: string): Promise<string>;
-  public setRaw(key: string, value: string): void;
-  public getAfterRefresh<T>(key: string): Promise<T>;
-  private storage?: Record<string, string>;
+  public get: <T>(key: string, defaultValue?: T) => T;
+  public set: <T>(key: string, value: T) => void;
+  public remove: (key: string) => void;
+  public clear: () => void;
+  public stringify: () => string;
+  public asyncGetRaw: <T>(key: string, defaultValue?: T) => Promise<string | null>;
+  public setRaw: (key: string, value: string) => void;
+  public getAfterRefresh: <T>(key: string) => Promise<T>;
 }
 
-const localStorageMod = await waitForModule(filters.bySource("delete window.localStorage"));
+const storageMod = await waitForModule(filters.bySource("delete window.localStorage"));
 
-export default getExportsForProps<LocalStorage>(localStorageMod, ["getAfterRefresh"])!;
+export default getExportsForProps<LocalStorage>(storageMod, ["getAfterRefresh"])!;
