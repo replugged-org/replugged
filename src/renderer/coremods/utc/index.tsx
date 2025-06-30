4837278f51d63b16400bac74f10a0b2f36c4c5d2
yofukashino <alokender018@gmail.com>
2025-06-30 08:07:55 +0530

    init: coordinated universal theming

---
+++ src/renderer/coremods/utc/index.tsx
@@ -0,0 +1,23 @@
+type RegExpReducible<T> = RegExpStringIterator<T> & { reduce: T[]["reduce"] };
+const classMap = new Map<string, string>();
+
+/**
+ * @internal
+ * @hidden
+ */
+
+export function _getUnifiedClassName(input: string): string {
+  if (classMap.has(input)) return classMap.get(input)!;
+
+  const baseClasses = input.includes("utc_") ? input.replaceAll(/utc_\S+\s*/g, "").trim() : input;
+
+  const suffixMatch = baseClasses.matchAll(/(\w+?)_/g) as RegExpReducible<RegExpExecArray>;
+
+  const suffix = suffixMatch.reduce((suffix, [_, name]) => `${suffix}_${name}`, "");
+
+  const unified = suffix.length ? `${baseClasses} utc${suffix}` : baseClasses;
+
+  classMap.set(input, unified);
+
+  return unified;
+}
---
+++ src/renderer/coremods/utc/plaintextPatches.ts
@@ -0,0 +1,14 @@
+import { type PlaintextPatch } from "src/types";
+
+export default [
+  {
+    find: `.jsx=`,
+    replacements: [
+      {
+        match: /return{\$\$typeof:\w+,type:(\w+).+?props:(\w+)/,
+        replace: (suffix, type, props) =>
+          ` ${props}.className && ${type} !== "html" && (${props}.className = replugged.coremods.coremods.utc?._getUnifiedClassName(${props}.className));${suffix}`,
+      },
+    ],
+  },
+] as PlaintextPatch[];
