08dcadbbc933cc4b3f9336d70086d4e316e9448c
yofukashino <alokender018@gmail.com>
2025-07-15 20:03:30 +0530

    fix: removing timing based listeners and return to monke (powercord)

--- src/main/win32updater.ts
+++ src/main/win32updater.ts
@@ -4,6 +4,7 @@ import { cpSync, existsSync, readdirSync, renameSync } from "fs";
 function getVersionFromName(name: string): number[] {
   return name.replace("app-", "").split(".").map(Number);
 }
+
 export default function addUpdateListener(): void {
   if (process.platform !== "win32") return;
   // app-x.x.x/resources/asar/app_bootstrap/index.js
@@ -11,30 +12,27 @@ export default function addUpdateListener(): void {
   const currentAsarDir = join(origAsarPath, "..", "app.asar");
   const currentVersion = basename(join(origAsarPath, "..", ".."));
   const discordPath = join(origAsarPath, "..", "..", "..");
-  const updater = require(join(origAsarPath, "common", "updater")).Updater;
+  const autoStartPath = join(origAsarPath, "autoStart", "index.js");
+  const { update } = require(autoStartPath);
 
-  const defaultUpdaterOn = updater.on.bind(updater);
-  updater.on = (event: string, cb?: () => unknown) => {
-    if (event !== "host-updated") return defaultUpdaterOn(event, cb);
-    defaultUpdaterOn(event, () => {
-      const newVersion = readdirSync(discordPath).reduce((oldVersionString, newVersionString) => {
-        if (!newVersionString.startsWith("app-")) return oldVersionString;
-        const oldVersion = getVersionFromName(oldVersionString);
-        const newVersion = getVersionFromName(newVersionString);
-        if (newVersion.some((n, i) => n > oldVersion[i])) return newVersionString;
-        return oldVersionString;
-      }, currentVersion);
+  require.cache[autoStartPath]!.exports.update = async (cb?: () => unknown) => {
+    const newVersion = readdirSync(discordPath).reduce((oldVersionString, newVersionString) => {
+      if (!newVersionString.startsWith("app-")) return oldVersionString;
+      const oldVersion = getVersionFromName(oldVersionString);
+      const newVersion = getVersionFromName(newVersionString);
+      if (newVersion.some((n, i) => n > oldVersion[i])) return newVersionString;
+      return oldVersionString;
+    }, currentVersion);
 
-      if (newVersion === currentVersion) return;
+    if (newVersion === currentVersion) return;
 
-      const newResources = join(discordPath, newVersion, "resources");
+    const newResources = join(discordPath, newVersion, "resources");
 
-      const asar = join(newResources, "app.asar");
-      const origAsar = join(newResources, "app.orig.asar");
-      if (!existsSync(origAsar)) renameSync(asar, origAsar);
+    const asar = join(newResources, "app.asar");
+    const origAsar = join(newResources, "app.orig.asar");
+    if (!existsSync(origAsar)) renameSync(asar, origAsar);
 
-      cpSync(currentAsarDir, asar, { recursive: true });
-      cb?.();
-    });
+    cpSync(currentAsarDir, asar, { recursive: true });
+    await update(cb);
   };
 }
