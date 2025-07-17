231752b65a0543425e228926b7e7e5a7aca15b46
yofukashino <alokender018@gmail.com>
2025-07-17 14:32:26 +0530

    style: add comments and easy to read

--- src/main/win32updater.ts
+++ src/main/win32updater.ts
@@ -6,15 +6,27 @@ function getVersionFromName(name: string): number[] {
   return name.replace("app-", "").split(".").map(Number);
 }
 
+function getPathBefore(path: string, before: string): string {
+  return join(path.substring(0, path.indexOf(before)));
+}
+
 export default function patchAutoStartUpdate(): void {
   const replugSetting = getSetting<boolean>("dev.replugged.Settings", "winUpdater", false);
   if (process.platform !== "win32" || !replugSetting) return;
-  // require.main!.filename -> %localappdata%/discord/app-x.x.x/resources/asar/app_bootstrap/index.js
-  const origAsarPath = join(require.main!.filename, "..", "..");
+
+  // require.main!.filename -> %localappdata%\discord\app-x.x.x\resources\app.orig.asar\app_bootstrap\index.js
+
+  // origAsarPath -> %localappdata%\discord\app-x.x.x\resources\app.orig.asar
+  const origAsarPath = getPathBefore(require.main!.filename, "\\app_bootstrap\\index");
+  // currentAsarDir -> %localappdata%/discord/app-x.x.x/resources/app.asar (folder)
   const currentAsarDir = join(origAsarPath, "..", "app.asar");
-  const currentVersion = basename(join(origAsarPath, "..", ".."));
-  const discordPath = join(origAsarPath, "..", "..", "..");
-  const autoStartPath = join(origAsarPath, "app_bootstrap", "autoStart", "index.js");
+  // currentVersion -> app-x.x.x
+  const currentVersion = basename(getPathBefore(origAsarPath, "\\resources\\app."));
+  // discordPath -> %localappdata%/discord
+  const discordPath = getPathBefore(origAsarPath, `${currentVersion}\\resources`);
+  // autoStartPath -> %localappdata%\discord\app-x.x.x\resources\app.orig.asar\app_bootstrap\autoStart\index.js
+  const autoStartPath = join(require.main!.filename, "..", "autoStart", "index.js");
+
   const { update } = require(autoStartPath);
 
   require.cache[autoStartPath]!.exports.update = async (cb?: () => unknown) => {
@@ -32,8 +44,9 @@ export default function patchAutoStartUpdate(): void {
 
     const asar = join(newResources, "app.asar");
     const origAsar = join(newResources, "app.orig.asar");
+    // rename app.asar -> app.orig.asar in newer app\x.x.x\resources
     if (!existsSync(origAsar)) renameSync(asar, origAsar);
-
+    // copy over the old app.asar from old app-x.x.x\resources
     cpSync(currentAsarDir, asar, { recursive: true });
     await update(cb);
   };
