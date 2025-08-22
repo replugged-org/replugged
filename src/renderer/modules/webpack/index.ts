347e97be7113d71efce889f603497bf940bce80f
Nanakusa <73281112+yofukashino@users.noreply.github.com>
2025-08-22 23:19:27 +0530

    feat: export wpRequire and sourceStrings (#734)

--- src/renderer/modules/webpack/index.ts
+++ src/renderer/modules/webpack/index.ts
@@ -10,3 +10,5 @@ export { getById, getExportsForProps, getModule } from "./get-modules";
 export * as filters from "./filters";
 
 export * from "./helpers";
+
+export { sourceStrings, wpRequire } from "./patch-load";
