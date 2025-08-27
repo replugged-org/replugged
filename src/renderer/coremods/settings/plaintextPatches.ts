4acacaecdbd9c25a3a051b3d7a8cb541c32121f2
yofukashino <alokender018@gmail.com>
2025-08-27 09:28:56 +0530

    feat: new plaintext patch

--- src/renderer/coremods/settings/plaintextPatches.ts
+++ src/renderer/coremods/settings/plaintextPatches.ts
@@ -4,12 +4,26 @@ const coremodStr = "replugged.coremods.coremods.settings";
 
 export default [
   {
-    find: "getPredicateSections",
+    find: "SettingsRendererConfig",
     replacements: [
       {
-        match: /(this\.props\.sections\.filter\(.+?\))}/,
-        replace: (_, filteredSections) =>
-          `${coremodStr}?.insertSections(${filteredSections}) ?? ${filteredSections}};`,
+        match: /return Object.freeze/,
+        replace: (_) => `return (${coremodStr}?.insertSections ?? ((obj)=>obj))`,
+      },
+    ],
+  },
+  {
+    find: `header:"Developer Only"`,
+    replacements: [
+      {
+        match: /(OVERLAY]};return )(\w+\?\w+:\w+.toSpliced\(3,0,\w+\))/,
+        replace: (_, prefix, records) =>
+          `${prefix}(${coremodStr}?.insertRecords(${records})??(${records}))`,
+      },
+      {
+        match: /((\w+\[\w+\])\),{tabPredicate:\(\)=>)(null==\w+\|\|\w+\(\w+,\w+,\w+\))/,
+        replace: (_, prefix, section, condition) =>
+          `${prefix}(!${section}?.tabPredicate||${section}?.tabPredicate?.())&&(${condition})`,
       },
     ],
   },
@@ -17,8 +31,14 @@ export default [
     find: ".versionHash",
     replacements: [
       {
-        match: /appArch,children:.{0,200}?className:\w+\.line,.{0,100}children:\w+}\):null/,
+        match:
+          /appArch,children:.{0,200}?className:\w+\(\)\(\w+\.line,\w+\.os\),.{0,100}children:\w+}\):null/,
         replace: `$&,${coremodStr}?.VersionInfo() ?? null`,
+      },
+      {
+        match: /\(0,\w+\.jsx\)\(\w+.\w+,{copyValue:(\w+)\.join/,
+        replace:
+          "$1.push(window.replugged.common.i18n.intl.format(window.replugged.i18n.t.REPLUGGED_VERSION,{version: window.RepluggedNative.getVersion()})),$&",
       },
     ],
   },
