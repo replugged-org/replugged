2aec14ff6b21fa2926bf782edf47199db81c7bd0
yofukashino <alokender018@gmail.com>
2025-09-29 17:40:21 +0530

    chore: refactor

--- src/renderer/coremods/settings/plaintextPatches.ts
+++ src/renderer/coremods/settings/plaintextPatches.ts
@@ -17,8 +17,7 @@ export default [
     find: ".versionHash",
     replacements: [
       {
-        match:
-          /appArch,children:.{0,200}?className:\i\(\)\(\i\.line,\i\.os\),.{0,100}children:\i}\):null/,
+        match: /\i\.line,\i\.os\),.{0,100}children:\i}\):null/,
         replace: `$&,${coremodStr}?.VersionInfo() ?? null`,
       },
       {
