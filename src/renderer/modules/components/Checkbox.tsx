e25bb27546f9ed64912a90ff9813da5c5e529ffd
Federico <38290480+fedeericodl@users.noreply.github.com>
2025-10-25 20:36:48 +0200

    feat: wrap Checkbox to support the prop value

--- src/renderer/modules/components/Checkbox.tsx
+++ src/renderer/modules/components/Checkbox.tsx
@@ -1,3 +1,18 @@
+import type React from "react";
 import components from "../common/components";
 
-export default components.Checkbox;
+import type * as Design from "discord-client-types/discord_app/design/web";
+
+const { Checkbox } = components;
+
+type CustomCheckboxProps = Design.CheckboxProps & {
+  value?: boolean;
+};
+
+export type CustomCheckboxType = React.FC<CustomCheckboxProps>;
+
+function CustomCheckbox({ value, ...props }: CustomCheckboxProps): React.ReactElement {
+  return <Checkbox checked={value} {...props} />;
+}
+
+export default CustomCheckbox;
