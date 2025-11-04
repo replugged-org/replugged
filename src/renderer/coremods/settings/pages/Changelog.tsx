ae7b5f1f73a71874c9694cede171a0fcda0fffc8
yofukashino <alokender018@gmail.com>
2025-11-04 17:48:54 +0530

    feat: link preview just incase

--- src/renderer/coremods/settings/pages/Changelog.tsx
+++ src/renderer/coremods/settings/pages/Changelog.tsx
@@ -80,6 +80,7 @@ export const ChangelogModal = ({
             allowHeading: true,
             allowList: true,
             allowLinks: true,
+            previewLinkTarget: true,
           })}
         </Text>
       </Modal.ModalContent>
